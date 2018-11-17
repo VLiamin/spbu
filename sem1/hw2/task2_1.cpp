9cc7043818b52cb05d13ec5d90f0e93684782207
Vladimir Liamin <LyaminVA@yandex.ru>
2018-11-17 23:49:12 +0300

    variable names

--- sem1/hw2/task2_1.cpp
+++ sem1/hw2/task2_1.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-const int lengthArray = 10000;
 int reverse(int res);
 int iterative(int rate);
 	
@@ -8,29 +7,31 @@ main()
 	int number = 0;
 	printf("number = ");
 	scanf("%d", &number);
-	int rec = reverse(number);
-	int ite = iterative(number);
-	printf("result recurse = %d\n", rec);
-	printf("result iterative = %d", ite);
+	int resultReverse = reverse(number);
+	int resultIterative = iterative(number);
+	printf("result reverse = %d\n", resultReverse);
+	printf("result iterative = %d", resultIterative);
 	return 0;
 }
  
- int reverse(int res)
+int reverse(int numeral)
 {
-	if (res > 2)
-		return reverse(res - 1) + reverse(res - 2);
+	if (numeral > 2)
+		return reverse(numeral - 1) + reverse(numeral - 2);
 	else return 1;
 }
  
- int iterative(int rate)
+int iterative(int numeral)
 {
+	int *fib = new int[numeral];
 	int i = 0;
-	int fib[lengthArray];
 	fib[0] = 1;
-	fib[1] = 1;	
-	for (i = 2; i < rate; i++)
+	fib[1] = 1;
+	for (i = 2; i < numeral; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];	
 	}
-	return fib[i - 1];
+	i = fib[i - 1];
+	delete [] fib;
+	return i;
 }
