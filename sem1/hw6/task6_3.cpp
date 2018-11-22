87c4aa7ac1860dd1ba6329a15bedb307c1b6a8ef
VLiamin <LyaminVA@yandex.ru>
2018-11-22 17:42:00 +0300

    broke into functions

--- sem1/hw6/task6_3.cpp
+++ sem1/hw6/task6_3.cpp
@@ -1,73 +1,86 @@
 #include <stdio.h>
-main()
+void printPower(int power, int* expression);
+
+void printX(int j, int power);
+
+main(int argc, char *argv[])
 {
 	int power = 0; 
 	printf("Power: ");
 	scanf("%d", &power);
-	int *expression = new int(power);
+	int *expression = new int[power];
 	printf("Your expression: ");
 	int i = 0;
 	for (i = 0; i <= power; i++)
 	{
 		scanf("%d", &expression[i]);
 	}
-	for (i = 0; i < power; i++)
-	{
-		int substitute = expression[i];
-		if (substitute != 0)
-		{
-			if ((expression[0] < 0) && (i == 0))
-				printf(" ");
-			while ((substitute != 0) && (expression[i] != 1))
-			{
-				printf(" ");
-				substitute = int(substitute / 10);
-			}
-			printf(" ");
-			printf("%d", power - i);
-			printf("  ");
-		}
-	}
+	
+	printPower(power, expression);
+	
 	printf("\n");
 	for (i = 0; i <= power; i++)
 	{
-		int substitute = power - i;
 		if (expression[i] != 0)
 		{
 			if ((expression[i] > 0) || (i == 0))
 			{
 				if (i != 0)
 					printf("+ ");
-				if (expression[i] != 1)
-					printf("%d", expression[i]);
-				if (i != power)
-				{
-					printf("x");
-					while (substitute != 0)
-					{
-						printf(" ");
-						substitute = substitute / 10;
-					}
-				}
+				if ((expression[i] != 1) && (expression[i] != -1))
+					printf("%d", expression[i]);	
+				if ((expression[i] == -1) && (i == 0))
+					printf("-");
+						
+				printX(i, power);
+				
 			}
 			else
 			{
 				printf("- ");
-				if (expression[i] * (-1) != 1)
-					printf("%d", expression[i] * (-1));
-				if (i != power)
-				{
-					printf("x");
-					while (substitute != 0)
-					{
-						printf(" ");
-						substitute = substitute / 10;
-					}
-				}
+				printf("%d", expression[i] * (-1));
+				
+				printX(i, power);
+				
 			}
 			
 		}
 	}
 	delete [] expression;
 	return 0;
+}
+
+void printPower(int power, int* expression)
+{
+	for (int j = 0; j < power - 1; j++)
+	{
+		int replacement = expression[j];
+		if (replacement != 0)
+		{
+			if ((expression[0] < 0) && (j == 0))
+				printf(" ");
+			while ((replacement != 0) && (expression[j] != 1) && (expression[j] != -1))
+			{
+				printf(" ");
+				replacement = int(replacement / 10);
+			}
+			printf(" ");
+			printf("%d", power - j);
+			printf("  ");
+		}
+	}	
+}
+
+void printX(int j, int power)
+{
+	int substitute = power - j;
+	if (j != power)
+	{
+		printf("x");
+		while (substitute != 0)
+		{
+			printf(" ");
+			substitute = substitute / 10;
+		}
+	}
 }
