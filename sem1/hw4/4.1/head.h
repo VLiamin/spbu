2d8c64b9a7c51497db50ff6abcedabe9de5b0bf3
VLiamin <LyaminVA@yandex.ru>
2018-11-29 22:25:04 +0300

    spaces

--- sem1/hw4/4.1/head.h
+++ sem1/hw4/4.1/head.h
@@ -1,13 +1,14 @@
 #ifndef head_H
 #define head_H
 struct List
-	{
-		int number;
-		List *next;
-	};	
+{
+	int number;
+	List *next;
+};	
 
 List *createList();
 List *add(List *tmp, int number);
 List *kill(List *tmp, int i, int dead);
 void print(List *head);	
+
 #endif
