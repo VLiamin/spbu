2223ca8aa6c658bcb5d590149cd27d2b51e91a75
VLiamin <LyaminVA@yandex.ru>
2018-11-29 22:13:55 +0300

    head

---
+++ sem1/hw4/4.1/head.h
@@ -0,0 +1,13 @@
+#ifndef head_H
+#define head_H
+struct List
+	{
+		int number;
+		List *next;
+	};	
+
+List *createList();
+List *add(List *tmp, int number);
+List *kill(List *tmp, int i, int dead);
+void print(List *head);	
+#endif
