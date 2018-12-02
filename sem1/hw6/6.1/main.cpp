8030f532115a291eda7b09a55c4378da1eb84099
Vladimir Liamin <LyaminVA@yandex.ru>
2018-12-02 21:50:31 +0300

    main spaces

--- sem1/hw6/6.1/main.cpp
+++ sem1/hw6/6.1/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    double number = 0.0;
-    printf("Your number: ");
+	double number = 0.0;
+	printf("Your number: ");
 	scanf("%lf", &number);
-    printExponential(number);
-    return 0;
+	printExponential(number);
+	return 0;
 }
