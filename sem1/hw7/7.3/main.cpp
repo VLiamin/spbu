0adc2a68c45574f481d062e92e2e99b4ac3e424b
VLiamin <LyaminVA@yandex.ru>
2018-12-14 01:27:13 +0300

    main

---
+++ sem1/hw7/7.3/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "head.h"
+
+using namespace std;
+
+const int lengthArray = 100;
+
+int searchTokenEnd(char *string, int i); 
+
+int main(int argc, char *argv[])
+{
+    Tree *tree = createTree();
+    FILE *f = fopen("text7_3.txt", "r");
+    char string[lengthArray];
+    fgets(string, lengthArray, f);
+    push(tree, string, 0);
+    printf("Expression: ");
+    print(tree);
+    printf("\n");
+    printf("Value: %d", count(tree));
+    fclose(f);
+    return 0;
+}
+
+int searchTokenEnd(char *string, int i) 
+{
+    int sum = 0;
+    if (string[i] == '(') 
+	{
+        sum++;
+        i++;
+        while (sum != 0) 
+		{
+            if (string[i] == '(')
+                sum++;
+            else if (string[i] == ')')
+                sum--;
+            i++;
+        }
+    } 
+	else 
+	{
+        i++;
+    }
+
+    return i;
+}
