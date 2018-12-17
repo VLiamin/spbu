349e4357569bca69ff2204ff076027a80e30f60a
Vladimir Liamin <LyaminVA@yandex.ru>
2018-12-18 00:29:16 +0300

    tree announced

---
+++ sem1/hw9/9.2/tree.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "list.h"
+struct Node
+{
+	char symbol;
+	int value;                          
+	Node *left;
+	Node *right;  
+	Node *tree;                      
+};
+
+struct Tree
+{
+	Node *root;
+};
+
+Tree *createTree();
+void build(List *list, Tree *&tree);
+void printABC(Tree *tree);
+void coding(Tree *tree, List *list, long number);
