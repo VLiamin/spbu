f7d66a0590ca971bda1f8a7aa8289ffe563707f4
Vladimir Liamin <LyaminVA@yandex.ru>
2018-12-20 00:52:13 +0300

    implemented conversion from infix to postfix

--- sem1/hw5/task5.3/head.cpp
+++ sem1/hw5/task5.3/head.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "head.h"
 
 Stack *createStack()
@@ -40,10 +39,8 @@ char pop(Stack *element)
 
 bool check(StackElement *element)
 {
-	if (element != nullptr)
-	{
-		return true;
-	}
+	if (element != nullptr) return true;
+	
 	return false;
 }
 
@@ -54,8 +51,8 @@ bool check(Stack *stack)
 
 bool plus(StackElement *element)
 {
-	if ((element->value == '-') || (element->value == '+'))
-		return true;
+	if (element == nullptr)	return false;
+	if ((element->value == '-') || (element->value == '+')) return true;
 	return false;
 }
 
@@ -67,4 +64,118 @@ bool plus(Stack *stack)
 void deleteStack(Stack *stack)
 {
 	delete stack;
+}
+
+int countParentheses(char *expression)
+{
+	int parentheses = 0;
+	int i = 0;
+	while (expression[i] != '\n')
+	{
+		if (expression[i] == '(')
+		{
+			parentheses++;
+		}
+		i++;
+	}
+	int lengthString = i - parentheses * 2;
+	return lengthString;
+}
+
+void seeParentheses(Stack *stack, char *expression, int &i, char &symbol, int &parentheses, char *expressionResult, int &number)
+{
+	if (expression[i] == '(')
+	{
+		parentheses++;
+		if (symbol != 'b')
+		{
+			add(symbol, stack); 
+			symbol = 'b';
+		}
+		i++;
+
+	}
+	if (expression[i] == ')')
+	{
+		parentheses--;
+		if (symbol != 'b')
+		{
+			expressionResult[number] = symbol;
+			number++;
+		}
+		
+		if (plus(stack))
+		{
+			symbol = pop(stack);
+			i++;
+			return;
+		}
+
+		if (check(stack))
+		{
+			expressionResult[number] = pop(stack);
+			number++;
+		}
+		i++;
+	}
+}
+
+void swapSymbols(Stack *stack, char *expression, int &i, char &symbol, int &parentheses, char *expressionResult, int &number)
+{
+	if ((int(expression[i]) >= '0') && (int(expression[i]) <= '9'))
+	{
+		expressionResult[number] = expression[i];
+		i++;
+		number++;
+	}
+		
+	if ((expression[i] == '*') || (expression[i] == '/'))
+	{
+		if (expression[i + 1] != '(')
+		{
+		
+			expressionResult[number] = expression[i + 1];
+			expressionResult[number + 1] = expression[i];
+			number += 2;
+			i += 2;
+		}
+		else
+		{
+			add(expression[i], stack); 
+			i++;
+		}
+	}
+	if ((expression[i] == '-') || (expression[i] == '+'))
+	{
+		if (symbol != 'b')
+		{
+			expressionResult[number] = symbol;
+			number++;
+		}
+		symbol = expression[i];	
+		i++;
+	}
+}
+
+void tinkeringFromInfixToPostfix(Stack *stack, char *expression, char *expressionResult)
+{
+	int i = 0;
+	int number = 0;
+	int parentheses = 0;
+	char symbol= 'b';
+   
+	while (expression[i] != '\n')
+	{
+		seeParentheses(stack, expression, i, symbol, parentheses, expressionResult, number);
+		
+		swapSymbols(stack, expression, i, symbol, parentheses, expressionResult, number);
+			
+	}
+	
+	if ((symbol == '-') || (symbol == '+'))
+	{
+		expressionResult[number] = symbol;
+		symbol = 'b';
+		number++;
+	}
 }
