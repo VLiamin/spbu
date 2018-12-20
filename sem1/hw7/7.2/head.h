7221d32ba87bb41745066541ff2c722bf026bd90
Vladimir Liamin <LyaminVA@yandex.ru>
2018-12-20 21:31:53 +0300

    removed function

--- sem1/hw7/7.2/head.h
+++ sem1/hw7/7.2/head.h
@@ -21,13 +21,6 @@ void printDe(Tree *tree);
 bool found(int number, Tree *tree);
 void printABC(Tree *tree);
 unsigned char height(Tree* p);
-int bfactor(Node* p);
-void fixheight(Node* p);
-Node* rotateright(Node* p);
-Node* rotateleft(Node* q);
-Node* balance(Node* p);  
-Node* findmin(Node* p);
-Node* removemin(Node* p);
 void remove(Tree *&tree, int number);
 void deleteTree(Tree *tree);
 
