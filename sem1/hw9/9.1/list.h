53bf295d5cbb1eef4a0ce76c82a3a4e1100edeff
Vladimir Liamin <LyaminVA@yandex.ru>
2018-12-27 22:36:46 +0300

    added list.h

---
+++ sem1/hw9/9.1/list.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct ListElement
+{
+	ListElement *next;
+	int town;
+};
+
+struct List
+{
+	ListElement *element;
+};
+
+List *createList();
+void pushList(List *&list, int sity);
+int findTown(List *list, int number);
+void printList(List *list);
+void deleteOneElement(List *list, int **matrix, int element);
+void deleteList(List *list);
