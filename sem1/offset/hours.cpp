fc973f41917a3364f9722ff9c9e8490babb7a032
VLiamin <LyaminVA@yandex.ru>
2018-12-28 14:09:31 +0300

    added hours

---
+++ sem1/offset/hours.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+const int length = 24;
+int main()
+{
+	int minute = 0;
+	int hourStart = 0;
+	int hourFinish = 0;
+	int hours[length];
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		hours[i] = 0;
+	}
+	FILE *f = fopen("text.txt", "r");
+	while (!feof(f))
+	{
+		fscanf(f, "%d", &hourStart);
+		fscanf(f, "%d", &minute);
+		fscanf(f, "%d", &hourFinish);
+		fscanf(f, "%d", &minute);
+		for (i = hourStart; i <= hourFinish; i++)
+		{
+			hours[i]++;
+		}
+	}
+	int max = -1;
+	int maxHour = -1;
+	for (int i = 0; i < length; i++)
+	{
+		if (hours[i] > max)
+		{
+			max = hours[i];
+		}
+	}
+	for (int i = 0; i < length; i++)
+	{
+		if (hours[i] == max)
+		{
+			printf("maxHour: %d\n", i);
+		}
+	}
+	return 0;
+}
