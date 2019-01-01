4f6db43c81d2bf9961523087ca441cbeca20b395
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-01 18:57:28 +0300

    removed all unnecessary

--- sem1/hw5/task5.3/task5_3.cpp
+++ sem1/hw5/task5.3/task5_3.cpp
@@ -1,60 +1,50 @@
 #include <stdio.h>
 #include "head.h"
+#include "calculate.h"
 
-char* allocateMemory(char* string, int i);
+char* allocateMemory(char* str, int i);
 
 using namespace std;
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int length = 0;
-	printf("Your expression: ");
-	int number = 0;
-	char x = 'b';
-	int i = 0;
-	char *expression = new char[10];
-	scanf("%c", &x);
-	expression[i] = x;
-	
-	while (expression[i] != '\n')
-	{
-		i++;
-		scanf("%c", &x);
-		expression[i] = x;
-		if (i % 10 == 9)
-		{
-			allocateMemory(expression, i + 1);
-		}
-	}
-	
-	
-	Stack *stack = createStack();
-	i = 0;
-	number = 0;
-	
-	int lengthString = countParentheses(expression);
-	
-	char *expressionResult = new char[lengthString - 1];
-	
-	tinkeringFromInfixToPostfix(stack, expression, expressionResult);
-	
-	
-	printf("stringResult: ");
-	for (i = 0; i < lengthString; i++)
-		printf("%c", expressionResult[i]);
-	
-	delete [] expressionResult;
-	delete [] expression;
-	deleteStack(stack);
-	return 0;
+    int length = 0;
+    printf("Your expression: ");
+    char x = 'b';
+    int i = 0;
+    char *expression = new char[10];
+    scanf("%c", &x);
+    expression[i] = x;
+
+    while (expression[i] != '\n')
+    {
+        i++;
+        scanf("%c", &x);
+        expression[i] = x;
+        if (i % 10 == 9)
+        {
+            allocateMemory(expression, i + 1);
+        }
+    }
+
+    length = countPostfixLength(expression);
+
+    char* expressionResult = tinkeringFromInfixToPostfix(expression, length);
+
+    printf("stringResult: ");
+    print(expressionResult, length);
+
+    delete [] expressionResult;
+    delete [] expression;
+    return 0;
 }
 
-char* allocateMemory(char* string, int i)
+char* allocateMemory(char* str, int i)
 {
-	char *string2 = new char[i + 10];
-	for (int j = 0; j < i; j++)
-	{
-		string2[j] = string[j];
-	}
-	return string2;
+    char *string2 = new char[i + 10];
+    for (int j = 0; j < i; j++)
+    {
+        string2[j] = str[j];
+    }
+    return string2;
 }
