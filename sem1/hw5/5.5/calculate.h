a4ef2c13b64b5d2e0494cff965c1d53317e1a9cb
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-01 19:59:11 +0300

    added calculate.h

---
+++ sem1/hw5/5.5/calculate.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "head.h"
+
+char* tinkeringFromInfixToPostfix(Stack *stack, char *expression, int length);
+
+int countPostfixLength(char *expression);
+
+int countExpression(char *string, int length);
+
