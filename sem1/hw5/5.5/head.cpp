3feb8be9bae535aeec7c162f73495689cd9320b1
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-01 19:57:52 +0300

    removed all unnecessary

--- sem1/hw5/5.5/head.cpp
+++ sem1/hw5/5.5/head.cpp
@@ -1,229 +1,67 @@
 #include "head.h"
 
-Stack *createStack()
-{
-	return new Stack {nullptr};
+Stack *createStack() {
+    return new Stack{nullptr};
 }
 
-void add(char x, StackElement *&element)                          
-{
-	StackElement *temp = new StackElement;                             
-	temp->value = x;                                        
-	temp->next = element;                          
-	element = temp; 
-	                               
+void add(char x, StackElement *&element) {
+    StackElement *temp = new StackElement;
+    temp->value = x;
+    temp->next = element;
+    element = temp;
+
 }
 
-void add(char x, Stack *&stack)
-{
-	add(x, stack->head);
+void add(char x, Stack *&stack) {
+    add(x, stack->head);
 }
 
-char pop(StackElement *&element)
-{
-	if (element != nullptr)                        
-	{
-		char value = element->value;
-		StackElement *temp = element->next;                    
-		delete element;                                
-		element = temp;  
-		return value;                              
-	}
-	return 'b';
+char pop(StackElement *&element) {
+    if (element != nullptr) {
+        char value = element->value;
+        StackElement *temp = element->next;
+        delete element;
+        element = temp;
+        return value;
+    }
+    return 'b';
 }
 
-char pop(Stack *element)
-{
-	return pop(element->head);
+char pop(Stack *element) {
+    return pop(element->head);
 }
 
-bool check(StackElement *element)
-{
-	if (element != nullptr) return true;
-	return false;
+bool isNotEmpty(StackElement *element) {
+    if (element != nullptr) return true;
+
+    return false;
 }
 
-bool check(Stack *stack)
-{
-	return check(stack->head);
+bool isNotEmpty(Stack *stack) {
+    return isNotEmpty(stack->head);
 }
 
-bool plus(StackElement *element)
-{
-	if (!element) return false;
-	if ((element->value == '-') || (element->value == '+')) return true;
-	return false;
+bool isPlusOrMinus(StackElement *element) {
+    if (element == nullptr) return false;
+    if ((element->value == '-') || (element->value == '+')) return true;
+    return false;
 }
 
-bool plus(Stack *stack)
-{
-	return plus(stack->head);
+bool isPlusOrMinus(Stack *stack) {
+    return isPlusOrMinus(stack->head);
 }
 
-void deleteStack(StackElement *element)
-{
-	StackElement *tmp = element;
-	while (element)
-	{
-		element = element->next;
-		delete tmp;
-		tmp = element;
-	}
+void deleteStack(StackElement *element) {
+    StackElement *tmp = element;
+    while (element) {
+        element = element->next;
+        delete tmp;
+        tmp = element;
+    }
 }
 
-void deleteStack(Stack *stack)
-{
-	deleteStack(stack->head);
-	delete stack;
+void deleteStack(Stack *stack) {
+    deleteStack(stack->head);
+    delete stack;
 }
 
-int countParentheses(char *expression)
-{
-	int parentheses = 0;
-	int i = 0;
-	while (expression[i] != '\n')
-	{
-		if (expression[i] == '(')
-		{
-			parentheses++;
-		}
-		i++;
-	}
-	int lengthString = i - parentheses * 2;
-	return lengthString;
-}
-
-void seeParentheses(Stack *stack, char *expression, int &i, char &symbol, int &parentheses, char *expressionResult, int &number)
-{
-	if (expression[i] == '(')
-	{
-		parentheses++;
-		if (symbol != 'b')
-		{
-			add(symbol, stack); 
-			symbol = 'b';
-		}
-		i++;
-
-	}
-	if (expression[i] == ')')
-	{
-		parentheses--;
-		if (symbol != 'b')
-		{
-			expressionResult[number] = symbol;
-			number++;
-		}
-		
-		if (plus(stack))
-		{
-			symbol = pop(stack);
-			i++;
-			return;
-		}
-
-		if (check(stack))
-		{
-			expressionResult[number] = pop(stack);
-			number++;
-		}
-		i++;
-	}
-}
-
-void swapSymbols(Stack *stack, char *expression, int &i, char &symbol, int &parentheses, char *expressionResult, int &number)
-{
-	if ((int(expression[i]) >= '0') && (int(expression[i]) <= '9'))
-	{
-		expressionResult[number] = expression[i];
-		i++;
-		number++;
-	}
-		
-	if ((expression[i] == '*') || (expression[i] == '/'))
-	{
-		if (expression[i + 1] != '(')
-		{
-		
-			expressionResult[number] = expression[i + 1];
-			expressionResult[number + 1] = expression[i];
-			number += 2;
-			i += 2;
-		}
-		else
-		{
-			add(expression[i], stack); 
-			i++;
-		}
-	}
-	if ((expression[i] == '-') || (expression[i] == '+'))
-	{
-		if (symbol != 'b')
-		{
-			expressionResult[number] = symbol;
-			number++;
-		}
-		symbol = expression[i];	
-		i++;
-	}
-}
-
-void tinkeringFromInfixToPostfix(Stack *stack, char *expression, char *expressionResult)
-{
-	int i = 0;
-	int number = 0;
-	int parentheses = 0;
-	char symbol= 'b';
-   
-	while (expression[i] != '\n')
-	{
-		seeParentheses(stack, expression, i, symbol, parentheses, expressionResult, number);
-		
-		swapSymbols(stack, expression, i, symbol, parentheses, expressionResult, number);
-			
-	}
-	
-	if ((symbol == '-') || (symbol == '+'))
-	{
-		expressionResult[number] = symbol;
-		symbol = 'b';
-		number++;
-	}
-}
-
-void count(Stack *stack, char *string, int &meaning, int lengthString)
-{
-	int i = 0;
-	char a = '0';
-	char b = '0';
-	
-	while (i < lengthString)
-	{
-		while ((string[i] >= '0') && (string[i] <= '9'))
-		{
-			add(string[i], stack);
-			i++;
-		}
-
-		while ((i < lengthString) && ((string[i] < '0') || (string[i] > '9')))
-		{
-			a =  pop(stack); 
-			b =  pop(stack);
-			a = a - '0';
-			b = b - '0';
-        
-			if (string[i] == '-')
-				meaning = b - a;
-			else if (string[i] == '+')
-				meaning = b + a;
-			else if (string[i] == '*')
-				meaning = b * a;
-			else
-				meaning = int(b / a);
-        	
-			add(meaning + '0', stack);
-
-			i++;
-		}
-	}
-}
