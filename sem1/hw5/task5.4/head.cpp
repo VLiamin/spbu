9fc6697d8c400e77f248611a547d8a0c5ce66bb3
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-01 19:14:24 +0300

    removed all unnecessary

--- sem1/hw5/task5.4/head.cpp
+++ sem1/hw5/task5.4/head.cpp
@@ -49,39 +49,3 @@ void deleteStack(Stack *stack)
 	delete stack;
 }
 
-void count(Stack *stack, char *string, int &meaning)
-{
-	int i = 0;
-	char a = '0';
-	char b = '0';
-	
-	while (string[i] != '\n')
-	{
-		while ((string[i] >= '0') && (string[i] <= '9'))
-		{
-			add(string[i], stack);
-			i++;
-		}
-
-		while ((string[i] != '\n') && ((string[i] < '0') || (string[i] > '9')))
-		{
-			a =  pop(stack); 
-			b =  pop(stack);
-			a = a - '0';
-			b = b - '0';
-        
-			if (string[i] == '-')
-				meaning = b - a;
-			else if (string[i] == '+')
-				meaning = b + a;
-			else if (string[i] == '*')
-				meaning = b * a;
-			else
-				meaning = int(b / a);
-        	
-			add(meaning + '0', stack);
-
-			i++;
-		}
-	}
-}
