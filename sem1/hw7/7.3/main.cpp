2c70c503bb235eac565635ff25ecc23710d64760
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-01 23:07:36 +0300

    tree removal function added

--- sem1/hw7/7.3/main.cpp
+++ sem1/hw7/7.3/main.cpp
@@ -1,56 +1,23 @@
 #include <stdio.h>
-#include "head.h"
+#include "tree.h"
 
 using namespace std;
 
-const int lengthArray = 100;
+const int length = 100;
 
-int searchTokenEnd(char *string, int i); 
-
-int main(int argc, char *argv[])
-{
-	
-	Tree *tree = createTree();
-	FILE *f = fopen("text7_3.txt", "r");
-	char string[lengthArray];
-	fgets(string, lengthArray, f);
-	push(tree, string, 0);
-	printf("Expression: ");
-	print(tree);
-	printf("\n");
-	printf("Value: %d", count(tree));
-	fclose(f);
-	return 0;
+int main(int argc, char *argv[]) {
+    Tree *tree = createTree();
+    FILE *f = fopen("text.txt", "r");
+    char str[length];
+    fgets(str, length, f);
+    push(tree, str, 0);
+    printf("Expression: ");
+    print(tree);
+    printf("\n");
+    printf("Value: %d", count(tree));
+    deleteTree(tree);
+    fclose(f);
+    return 0;
 }
 
-int searchTokenEnd(char *string, int i) 
-{
-	int sum = 0;
-	if (string[i] == '(') 
-	{
-		sum++;
-		i++;
-		while (sum != 0) 
-		{
-			if (string[i] == '(')
-				sum++;
-			else if (string[i] == ')')
-				sum--;
-			i++;
-		}
-		while (string[i] == ' ')
-		{
-			i++;
-		}
-	} 
-	else 
-	{
-		i++;
-		while (string[i] == ' ')
-		{
-			i++;
-		}
-	}
 
-	return i;
-}
