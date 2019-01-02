5ce69e7b89ce2fd86b1377c1b04ea528a4478f72
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-02 22:23:44 +0300

    new formalization

--- sem1/hw9/9.1/capital.h
+++ sem1/hw9/9.1/capital.h
@@ -1,14 +1,18 @@
 #pragma once
+
 #include "list.h"
 
-struct Capital
-{
-	List **towns;
-	int count;
+struct Capital {
+    List **towns;
+    int count;
 };
 
-Capital *createCapital(int sity);
-void pushCapital(Capital *capital, int sity, int i);
+Capital *createCapital(int city);
+
+void pushCapital(Capital *capital, int city, int i);
+
 void printCapital(Capital *capital);
+
 void deleteOneElement(Capital *capital, int **matrix, int element);
+
 void deleteCapital(Capital *capital);
