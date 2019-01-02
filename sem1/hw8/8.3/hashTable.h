ebfd17ef074b59aefe19734cfa6cc38c3ff74796
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-02 18:39:22 +0300

    added struct Bucket

--- sem1/hw8/8.3/hashTable.h
+++ sem1/hw8/8.3/hashTable.h
@@ -1,21 +1,38 @@
 #pragma once
-#include "string.h"
-#include "list.h"
 
-struct HashTable
-{
-	List **elements;
-	int size;
-	int currentSize;
+#include "string.h"
+
+const int size = 1500;
+const int modulo = 41;
+const int multiplier = 256;
+
+struct Bucket {
+    String *word;
+    int repeats;
+    int probes;
+};
+
+struct HashTable {
+    Bucket **elements;
+    int size;
+    int currentSize;
 };
 
 HashTable *createHashTable();
+
 void push(HashTable *table, char *word);
+
 double countLoadFactor(HashTable *table);
+
 double returnChainLength(HashTable *table);
+
 int countLoadWords(HashTable *table);
+
 int countEmptyCells(HashTable *table);
+
 void countEachWord(HashTable *table);
+
 int returnMaxChainLength(HashTable *table);
+
 void deleteHashTable(HashTable *table);
 
