74bbe4533ab66b4ee4d89d1dfa9b74024dd02db2
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-02 22:25:41 +0300

    fixed delete error

--- sem1/hw9/9.1/capital.cpp
+++ sem1/hw9/9.1/capital.cpp
@@ -1,46 +1,38 @@
 #include "capital.h"
-#include "list.h"
 #include <stdio.h>
 
-Capital *createCapital(int sity)
-{
-	Capital *capital = new Capital;
-	capital->towns = new List* [sity];
-	capital->count = sity;
-	for (int i = 0; i < sity; i++)
-	{
-		capital->towns[i] = createList();
-	}
-	return capital;
+Capital *createCapital(int city) {
+    Capital *capital = new Capital;
+    capital->towns = new List *[city];
+    capital->count = city;
+    for (int i = 0; i < city; i++) {
+        capital->towns[i] = createList();
+    }
+    return capital;
 }
 
-void pushCapital(Capital *capital, int sity, int i)
-{
-	pushList(capital->towns[i], sity);
+void pushCapital(Capital *capital, int city, int i) {
+    pushList(capital->towns[i], city);
 }
 
-void printCapital(Capital *capital)
-{
-	for (int i = 0; i < capital->count; i++)
-	{
-		printf("%d country: ", i + 1);
-		printList(capital->towns[i]);
-		printf("\n");
-	}
+void printCapital(Capital *capital) {
+    for (int i = 0; i < capital->count; i++) {
+        printf("%d country: ", i + 1);
+        printList(capital->towns[i]);
+        printf("\n");
+    }
 }
 
-void deleteOneElement(Capital *capital, int **matrix, int element)
-{
-	for (int i = 0; i < capital->count; i++)
-	{
-		deleteOneElement(capital->towns[i], matrix, element);
-	}
+void deleteOneElement(Capital *capital, int **matrix, int element) {
+    for (int i = 0; i < capital->count; i++) {
+        deleteOneElement(capital->towns[i], matrix, element);
+    }
 }
-void deleteCapital(Capital *capital)
-{
-	for (int i = 0; i < capital->count; i++)
-	{
-		deleteList(capital->towns[i]);
-	}
-	delete capital;
+
+void deleteCapital(Capital *capital) {
+    for (int i = 0; i < capital->count; i++) {
+        deleteList(capital->towns[i]);
+    }
+    delete capital->towns;
+    delete capital;
 }
