88de173c23f39b11611c59a16f551e611a16d6cb
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-02 22:22:29 +0300

    new formalization

--- sem1/hw9/9.1/list.cpp
+++ sem1/hw9/9.1/list.cpp
@@ -1,90 +1,73 @@
 #include "list.h"
 #include <stdio.h>
 
-List *createList()
-{
-	return new List {nullptr};
+List *createList() {
+    return new List{nullptr};
 }
 
-void pushList(ListElement *&element, int sity)
-{
-	if (!element)
-	{
-		element = new ListElement;
-		element->next = nullptr;
-		element->town = sity;
-		return;
-	}
-	pushList(element->next, sity);
+void pushList(ListElement *&element, int sity) {
+    if (!element) {
+        element = new ListElement;
+        element->next = nullptr;
+        element->town = sity;
+        return;
+    }
+    pushList(element->next, sity);
 }
 
-void pushList(List *&list, int sity)
-{
-	pushList(list->element, sity);
+void pushList(List *&list, int sity) {
+    pushList(list->element, sity);
 }
 
-int findTown(ListElement *element, int number)
-{
-	
-	if (number == -1)
-	{
-		return element->town;
-	}
-	while ((number != element->town) && (element->next != nullptr))
-	{
-		element = element->next;
-	}
-	if (element->next == nullptr)
-		return -1;
-	return element->next->town;
+int findTown(ListElement *element, int number) {
+
+    if (number == -1) {
+        return element->town;
+    }
+    while ((number != element->town) && (element->next != nullptr)) {
+        element = element->next;
+    }
+    if (element->next == nullptr)
+        return -1;
+    return element->next->town;
 }
 
-int findTown(List *list, int number)
-{
-	return findTown(list->element, number);
+int findTown(List *list, int number) {
+    return findTown(list->element, number);
 }
 
-void printList(ListElement *list)
-{
-	while (list)
-	{
-		printf("%d", list->town);
-		list = list->next;
-	}
+void printList(ListElement *list) {
+    while (list) {
+        printf("%d ", list->town);
+        list = list->next;
+    }
 }
 
-void printList(List *list)
-{
-	printList(list->element);
+void printList(List *list) {
+    printList(list->element);
 }
 
-void deleteOneElement(ListElement *list, int **matrix, int element)
-{
-	while (list)
-	{
-		matrix[element][list->town - 1] = -1;
-		matrix[list->town - 1][element] = -1;
-		list = list->next;
-	}
+void deleteOneElement(ListElement *list, int **matrix, int element) {
+    while (list) {
+        matrix[element][list->town - 1] = -1;
+        matrix[list->town - 1][element] = -1;
+        list = list->next;
+    }
 }
 
-void deleteOneElement(List *list, int **matrix, int element)
-{
-	deleteOneElement(list->element, matrix, element);
+void deleteOneElement(List *list, int **matrix, int element) {
+    deleteOneElement(list->element, matrix, element);
 }
 
-void deleteList(ListElement *element)
-{
-	while (element)
-	{
-		ListElement *current = element;
-		element = element->next;
-		delete current;
-	}
+void deleteList(ListElement *element) {
+    while (element) {
+        ListElement *current = element;
+        element = element->next;
+        delete current;
+    }
 }
 
-void deleteList(List *list)
-{
-	deleteList(list->element);
-	delete list;
+void deleteList(List *list) {
+    deleteList(list->element);
+    delete list;
 }
