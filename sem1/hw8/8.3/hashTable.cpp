8eb5ee6283f712a09bae0c29b4677c79f68707c9
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-02 18:41:00 +0300

    fixed error delete hashTable

--- sem1/hw8/8.3/hashTable.cpp
+++ sem1/hw8/8.3/hashTable.cpp
@@ -1,123 +1,124 @@
 #include "hashTable.h"
-#include "list.h"
 #include <iostream>
-#include <stdio.h>
-#include "string.h"
 
-const int size = 1500;
-const int divisor = 41;
-const int multiplier = 256;
+HashTable *createHashTable() {
+    HashTable *newTable = new HashTable;
+    newTable->size = size;
+    newTable->currentSize = 0;
+    newTable->elements = new Bucket *[size];
+    for (int i = 0; i < size; i++) {
+        newTable->elements[i] = new Bucket{nullptr, 0, 0};
 
-HashTable *createHashTable()
-{
-	HashTable *newTable = new HashTable;
-	newTable->size = size;
-	newTable->currentSize = 0;
-	newTable->elements = new List* [size];
-	for (int i = 0; i < size; i++)
-	{
-		newTable->elements[i] = createList();
-	}
-	return newTable;
+    }
+    return newTable;
 }
 
-int countHash(char *word)
-{
-	int i = 0;
-	long hash = 0;
-	printf("%s", word);
-	while (word[i] != '\0')
-	{
-		hash = (hash * multiplier + 'word[i]') % divisor + 1;
-		i++;
-	}
-	return hash;
+int countHash(char *word) {
+    int i = 0;
+    long hash = 0;
+    while (word[i] != '\0') {
+        hash = (hash * multiplier + word[i]) % modulo + 1;
+        i++;
+    }
+    return hash;
 }
 
-void push(HashTable *table, char *word)
-{
-	int hash = countHash(word);
-	int i = 1;
-	int number = 1;
-	while (!add(table->elements[hash + i * 2], word, table->currentSize, number))
-	{	
-		i = i * 2;
-	}
+bool writeWord(int &probes, Bucket *&bucket, char *word, int &currentSize, int &number) {
+    if (!bucket->word) {
+        int i = 0;
+        probes = number;
+        bucket->repeats = 1;
+        bucket->word = createString();
+        add(bucket->word, word, i);
+        currentSize++;
+        return true;
+    } else {
+        if (compare(bucket->word, word)) {
+            bucket->repeats++;
+            return true;
+        }
+        number++;
+        return false;
+    }
 }
 
-double countLoadFactor(HashTable *table)
-{
-	return (double) table->currentSize / table->size;
+void push(HashTable *table, char *word) {
+    int hash = countHash(word);
+    int i = 1;
+    int number = 1;
+    while (!writeWord(table->elements[hash + i * 2]->probes, table->elements[hash + i * 2], word, table->currentSize,
+                      number)) {
+        i = i * 2;
+
+    }
 }
 
-double returnChainLength(HashTable *table)
-{
-	int result = 0;
-	for (int i = 0; i < table->size; i++)
-	{
-		if (table->elements[i])
-		{
-			result = result + table->elements[i]->size;
-		}
-	}
-	return (double) result / table->currentSize;
+double countLoadFactor(HashTable *table) {
+    return (double) table->currentSize / table->size;
 }
 
-int countLoadWords(HashTable *table)
-{
-	return table->currentSize;
+double returnChainLength(HashTable *table) {
+    int result = 0;
+    for (int i = 0; i < table->size; i++) {
+        if (table->elements[i]) {
+            result = result + table->elements[i]->probes;
+        }
+    }
+    return (double) result / table->currentSize;
 }
 
-int countEmptyCells(HashTable *table)
-{
-	return table->size - table->currentSize;
+int countLoadWords(HashTable *table) {
+    return table->currentSize;
 }
 
-void countEachWord(HashTable *table)
-{
-	
-	for (int i = 0; i < table->size; i++)
-	{
-		if (table->elements[i]->first)
-		{
-			print(table->elements[i]->first->word);
-			printf(" - %d", table->elements[i]->first->number);
-			printf("\n");
-		}
-	}
+int countEmptyCells(HashTable *table) {
+    return table->size - table->currentSize;
 }
 
-void printChain(HashTable *table, int number)
-{
-	printf("Max example: ");
-	print(table->elements[number]->first->word);
-	printf("\n");
+void countEachWord(HashTable *table) {
+
+    for (int i = 0; i < table->size; i++) {
+        if (table->elements[i]->word) {
+            print(table->elements[i]->word);
+            printf(" - %d", table->elements[i]->repeats);
+            printf("\n");
+        }
+    }
 }
 
-int returnMaxChainLength(HashTable *table)
-{
-	int result = 0;
-	int number = 0;
-	for (int i = 0; i < table->size; i++)
-	{	
-		if (table->elements[i]->first)
-		{
-			if (table->elements[i]->first->number > result)
-			{
-				result = table->elements[i]->size;
-				number = i;
-			}
-		}
-	}
-	printChain(table, number);
-	return result;
+void printChain(HashTable *table, int number) {
+    int i = 0;
+    printf("Max example: ");
+    while (i < size) {
+        if ((table->elements[i]) && (table->elements[i]->probes == number)) {
+            print(table->elements[i]->word);
+            printf(" ");
+        }
+        i++;
+    }
+    printf("\n");
 }
 
-void deleteHashTable(HashTable *table)
-{
-	for (int i = 0; i < table->size; i++)
-	{
-		deleteList(table->elements[i]);
-	}
-	delete table;
+int returnMaxChainLength(HashTable *table) {
+    int result = 0;
+    int number = 0;
+    for (int i = 0; i < size; i++) {
+        if (table->elements[i]) {
+            if (table->elements[i]->probes > result) {
+                result = table->elements[i]->probes;
+            }
+        }
+    }
+    printChain(table, result);
+    return result;
+}
+
+void deleteHashTable(HashTable *table) {
+    for (int i = 0; i < table->size; i++) {
+        if (table->elements[i]->word) {
+            deleteString(table->elements[i]->word);
+        }
+        delete table->elements[i];
+    }
+    delete table;
 }
