49d60b3433ad1225a62fab492d2b11cf6a632e7f
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-03 14:49:36 +0300

    new design

--- sem1/hw9/9.2/list.h
+++ sem1/hw9/9.2/list.h
@@ -1,22 +1,30 @@
 #pragma once
 
-struct ListElement
-{
-	char symbol;
-	int number;
-	ListElement *next;
+struct ListElement {
+    char symbol;
+    int frequency;
+    int code;
+    ListElement *next;
 };
 
-struct List
-{
-	ListElement *first;
+struct List {
+    ListElement *first;
 };
 
 List *createList();
-void push(List *&list, char element, long number);
+
+void push(List *&list, char element, int frequency, long code);
+
 void sort(List *list);
+
 void deleteList(List *list);
+
 bool check(List *list, int &number);
+
 char pop(List *list, int &number);
+
 void write(List *list, char symbol);
+
 void deleteList(List *list);
+
+void printFrequency(List *list);
