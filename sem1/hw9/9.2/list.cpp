0cec70aa3ed9f0e8664d6f1330211b7085b60a6b
Vladimir Liamin <LyaminVA@yandex.ru>
2019-01-03 14:50:36 +0300

    delete list

--- sem1/hw9/9.2/list.cpp
+++ sem1/hw9/9.2/list.cpp
@@ -1,106 +1,109 @@
 #include <stdio.h>
 #include "list.h"
 
-List *createList()
-{
-	return new List {nullptr};
+List *createList() {
+    return new List{nullptr};
 }
 
-void push(ListElement *&tmp, char element, long number) 
-{
-	if (tmp == nullptr)
-	{
-		tmp = new ListElement;
-		tmp->next = nullptr;
-		tmp->number = number;
-		tmp->symbol = element;
-		return;
-	}
-	if (tmp->symbol == element)
-	{
-		tmp->number++;
-		return;
-	}
-	else
-	{
-		push(tmp->next, element, number);
-		return;
-	}
+void push(ListElement *&tmp, char element, int frequency, long code) {
+    if (tmp == nullptr) {
+        tmp = new ListElement;
+        tmp->next = nullptr;
+        tmp->symbol = element;
+        tmp->frequency = frequency;
+        tmp->code = code;
+        return;
+    } else if (tmp->symbol == element) {
+        tmp->frequency++;
+        return;
+    } else {
+        push(tmp->next, element, frequency, code);
+        return;
+    }
 }
 
-void push(List *&list, char element, long number)
-{
-	return push(list->first, element, number);
+void push(List *&list, char element, int frequency, long code) {
+    return push(list->first, element, frequency, code);
 }
 
-void sort(ListElement *first)
-{
-	ListElement *tmp2 = first;
-	ListElement *tmp = first;
-	while (first)
-	{
-		while (tmp)
-		{
-			if (first->number > tmp->number)
-			{
-				int number = first->number;
-				first->number = tmp->number;
-				tmp->number = number;
-				char symbol = first->symbol;
-				first->symbol = tmp->symbol;
-				tmp->symbol = symbol;
-			}
-			tmp = tmp->next;
-		}
-		first = first->next;
-		tmp = first;
-	}
+void sort(ListElement *first) {
+    ListElement *tmp2 = first;
+    ListElement *tmp = first;
+    while (first) {
+        while (tmp) {
+            if (first->frequency > tmp->frequency) {
+                int number = first->frequency;
+                first->frequency = tmp->frequency;
+                tmp->frequency = number;
+                char symbol = first->symbol;
+                first->symbol = tmp->symbol;
+                tmp->symbol = symbol;
+            }
+            tmp = tmp->next;
+        }
+        first = first->next;
+        tmp = first;
+    }
 }
 
-void sort(List *list)
-{
-	sort(list->first);
+void sort(List *list) {
+    sort(list->first);
 }
 
-bool check(List *list, int &number)
-{
-	if (list->first != nullptr)
-	{
-		number = list->first->number;
-		return true;
-	}
-	return false;
+bool check(List *list, int &number) {
+    if (list->first != nullptr) {
+        number = list->first->frequency;
+        return true;
+    }
+    return false;
 }
 
-char pop(ListElement *&first, int &number)
-{	
-	ListElement *tmp = first->next;	
-	char element = first->symbol;
-	number = first->number;
-	delete first;
-	first = tmp;
-	return element;
+char pop(ListElement *&first, int &number) {
+    ListElement *tmp = first->next;
+    char element = first->symbol;
+    number = first->frequency;
+    delete first;
+    first = tmp;
+    return element;
 }
 
-char pop(List *list, int &number)
-{
-	return pop(list->first, number);
+char pop(List *list, int &number) {
+    return pop(list->first, number);
 }
 
-void write(ListElement *list, char symbol)
-{
-	while (list->symbol != symbol)
-	{
-		list = list->next;
-	}
-	printf(" %d", list->number);
-}
-void write(List *list, char symbol)
-{
-	write(list->first, symbol);
+void write(ListElement *list, char symbol) {
+    while (list->symbol != symbol) {
+        list = list->next;
+    }
+    printf(" %d", list->code);
 }
 
-void deleteList(List *list)
-{
-	delete list;
+void write(List *list, char symbol) {
+    write(list->first, symbol);
+}
+
+void deleteList(ListElement *element) {
+    ListElement *tmp = element;
+    while (element) {
+        tmp = element;
+        element = element->next;
+        delete tmp;
+    }
+}
+
+void deleteList(List *list) {
+    deleteList(list->first);
+    delete list;
+}
+
+void printFrequency(ListElement *element) {
+    while (element) {
+        printf("\n");
+        printf("%c -- %d", element->symbol, element->frequency);
+        element = element->next;
+    }
+}
+
+void printFrequency(List *list) {
+    printFrequency(list->first);
 }
