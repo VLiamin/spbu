69888ffc23b965060a2cfe44da4c928474ca615e
VLiamin <LyaminVA@yandex.ru>
2019-02-28 20:05:57 +0300

    added bubble test

---
+++ sem2/hw2/task2_1/BubbleTest.java
@@ -0,0 +1,16 @@
+package ru.liamin.vladimir;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleTest {
+
+    @Test
+    void run() {
+        Bubble bubble = new Bubble();
+        int[] arrayOfNumbers = {2, 4, 1, 3, 7};
+        int[] sortedArrayOfNumbers = {7, 4, 3, 2, 1};
+        assertArrayEquals(sortedArrayOfNumbers,bubble.run(arrayOfNumbers));
+    }
+}
