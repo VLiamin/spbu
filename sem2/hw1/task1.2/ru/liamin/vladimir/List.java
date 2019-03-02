1d1b1adc5b3715761a31e1de175b656da861121c
Vladimir Liamin <LyaminVA@yandex.ru>
2019-03-02 21:51:11 +0300

    Changed single-line comments and made variables closed

--- sem2/hw1/task1.2/ru/liamin/vladimir/List.java
+++ sem2/hw1/task1.2/ru/liamin/vladimir/List.java
@@ -1,32 +1,23 @@
 package ru.liamin.vladimir;
 
-/**
- * Implementation of a unique list
- */
+/** Implementation of a unique list */
 public class List {
     private int size;
     private ListElement top;
 
     private class ListElement {
-        protected int value;
-        protected ListElement next;
+        private int value;
+        private ListElement next;
 
         public ListElement(int number, ListElement next) {
-            this.value = number;
+            value = number;
             this.next = next;
         }
     }
 
     /**
-     * Constructor of class
-      */
-    public List() {
-        size = 0;
-        top = null;
-    }
-
-    /**
      * Writing to the list
+     *
      * @param value record value
      * @param index index of an element
      */
@@ -51,38 +42,36 @@ public class List {
 
     /**
      * Returns the value and deletes the element
+     *
      * @param index index of an element
      * @return return value
      */
     public int pop(int index) {
-        if (index > size || index < 0)
-        {
+        if (index > size || index < 0) {
             System.out.println("Not found");
             return 0;
         }
 
-        if (index == 0)
-        {
+        if (index == 0) {
             int number = top.value;
             top = top.next;
             return number;
         }
-        else {
-            ListElement temp = top;
+        ListElement temp = top;
 
-            for (int i = 0; i < index - 1; i++) {
-                temp = temp.next;
-            }
-
-            int number = temp.next.value;
-            temp.next = temp.next.next;
-            return number;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
         }
+
+        int number = temp.next.value;
+        temp.next = temp.next.next;
+        return number;
 
     }
 
     /**
      * Check for emptiness
+     *
      * @return return true if list is empty
      */
     public boolean isEmpty() {
@@ -91,15 +80,14 @@ public class List {
 
     /**
      * Count of elements in list
+     *
      * @return number of elements in list
      */
     public int count() {
         return size;
     }
 
-    /**
-     * Print list elements
-     */
+    /** Print list element */
     public void printList() {
         System.out.println("Elements of list: ");
         ListElement current = top;
@@ -109,9 +97,7 @@ public class List {
         }
     }
 
-    /**
-     * Clear list
-     */
+    /** Clear list */
     public void clear() {
         size = 0;
         top = null;
@@ -120,6 +106,7 @@ public class List {
 
     /**
      * Demonstrates ru.liamin.vladimir.List methods
+     *
      * @param args array of arguments
      */
     public static void main(String[] args) {
