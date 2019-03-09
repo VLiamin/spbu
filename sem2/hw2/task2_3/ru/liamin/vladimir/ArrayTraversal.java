718db8193b7dfb78ed1611cb28a41060a1427fd8
Vladimir Liamin <LyaminVA@yandex.ru>
2019-03-09 14:01:16 +0300

    added ArrayTraversal.java

---
+++ sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversal.java
@@ -0,0 +1,56 @@
+package ru.liamin.vladimir;
+
+/** Loop traversal class */
+public class ArrayTraversal {
+    private int column;
+    private int lines;
+    private int j;
+    private int count;
+
+    /**
+     * Method that writes elements from the matrix to the array
+     * @param matrix source matrix of elements
+     * @return array in which matrix elements are stored
+     */
+    public int[] ArrayTraversal(int[][] matrix){
+        int[] elements = new int[matrix.length * matrix.length];
+        column = Math.round(matrix.length / 2);
+        lines = column;
+        j = 0;
+        while ((column != 0) && (lines != 0))
+        {
+            elements[j] = matrix[lines][column];
+
+            j++;
+            column--;
+            elements[j] = matrix[lines][column];
+            j++;
+            for (int i = 0; i <= 0 + count * 2; i++)
+            {
+                lines++;
+                elements[j] = matrix[lines][column];
+                j++;
+            }
+            for (int i = 0; i <= 1 + count * 2; i++)
+            {
+                column++;
+                elements[j] = matrix[lines][column];
+                j++;
+            }
+            for (int i = 0; i <= 1 + count * 2; i++)
+            {
+                lines--;
+                elements[j] = matrix[lines][column];
+                j++;
+            }
+            for (int i = 0; i <= 1 + count * 2; i++)
+            {
+                column--;
+                elements[j] = matrix[lines][column];
+                j++;
+            }
+            count++;
+        }
+        return elements;
+    }
+}
