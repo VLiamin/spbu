684541237ef431bbe4a38c23b6d5cacc8f4e0d97
Vladimir Liamin <LyaminVA@yandex.ru>
2019-03-09 14:12:56 +0300

    added ArrayTraversalTest

---
+++ sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversalTest.java
@@ -0,0 +1,21 @@
+package ru.liamin.vladimir;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTraversalTest {
+
+    @Test
+    void arrayTraversal() {
+        ArrayTraversal arrayTraversal = new ArrayTraversal();
+        int[] elements = {2, 1, 2, 3, 4, 3, 2, 1, 0};
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                matrix[i][j] = i + j;
+            }
+        }
+        assertArrayEquals(elements, arrayTraversal.ArrayTraversal(matrix));
+    }
+}
