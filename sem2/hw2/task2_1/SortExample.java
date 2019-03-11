e8555d0f2b7fd11c97d02cdd19238236da495938
VLiamin <LyaminVA@yandex.ru>
2019-03-11 13:58:01 +0300

    added SortExample

---
+++ sem2/hw2/task2_1/SortExample.java
@@ -0,0 +1,35 @@
+package ru.liamin.vladimir;
+
+/**
+ * Sorting implementation example
+ */
+public class SortExample {
+
+    /**
+     * Entering elements and starting sorting
+     * @param args array of arguments
+     */
+    public static void main(String[] args) {
+
+        int[] arrayOfNumbers = {2, 4, 1, 3, 7};
+
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.print(arrayOfNumbers[i] + " ");
+        }
+        System.out.println();
+        System.out.print("\nBubble sort");
+        Bubble bubble = new Bubble();
+        bubble.run(arrayOfNumbers);
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.print(arrayOfNumbers[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println("\nInsert sorting");
+        Insert insert = new Insert();
+        insert.run(arrayOfNumbers);
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.print(arrayOfNumbers[i] + " ");
+        }
+    }
+}
