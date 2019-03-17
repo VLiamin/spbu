c39fe7efb8aae7811af2b50bce44a3a5b0ed50bc
Vladimir Liamin <LyaminVA@yandex.ru>
2019-03-17 23:29:28 +0300

    added ListExample

---
+++ sem2/hw4/task4_1/ru/liamin/vladimir/ListExample.java
@@ -0,0 +1,40 @@
+package ru.liamin.vladimir;
+
+/** List example class */
+public class ListExample {
+    /**
+     * List example method
+     * @param args args array of arguments
+     */
+    public static void main(String[] args) {
+        UniqueList<Integer> uniqueList = new UniqueList<>();
+        int curentSize = 0;
+        try {
+            curentSize = uniqueList.push(10, curentSize);
+            curentSize = uniqueList.push(10, curentSize);
+        } catch (ElementAlreadyExistsExeption e){
+            System.out.println("This item is already there.");
+        }
+        try {
+            curentSize = uniqueList.push(11, curentSize);
+        } catch (ElementAlreadyExistsExeption e){
+            System.out.println("This item is already there.");
+        }
+
+        uniqueList.printList();
+        try {
+            curentSize = uniqueList.delete(11, curentSize);
+        } catch (ElementDoesNotExistExeption e){
+            System.out.println("This item is not there.");
+        }
+
+        try {
+            curentSize = uniqueList.delete(11, curentSize);
+        } catch (ElementDoesNotExistExeption e){
+            System.out.println("This item is not there.");
+        }
+        System.out.println("Size of list: " + uniqueList.count());
+        System.out.println(uniqueList.isEmpty());
+        uniqueList.clear();
+    }
+}
