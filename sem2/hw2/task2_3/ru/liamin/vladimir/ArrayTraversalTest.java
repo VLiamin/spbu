078b47be2f276c9b772bcf28b7d2b2b8f39cb3de
VLiamin <LyaminVA@yandex.ru>
2019-04-10 23:50:51 +0300

    additional array for recording matrix elements in ArrayTraversalTest.java

--- sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversalTest.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversalTest.java
@@ -10,12 +10,14 @@ class ArrayTraversalTest {
     void arrayTraversal() {
         ArrayTraversal arrayTraversal = new ArrayTraversal();
         int[] elements = {2, 1, 2, 3, 4, 3, 2, 1, 0};
+        int[] matrixElements = new int[9];
         int[][] matrix = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                matrix[i][j] = j + i;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                matrix[i][j] = i + j;
             }
         }
-        assertArrayEquals(elements, arrayTraversal.ArrayTraversal(matrix));
+        arrayTraversal.arrayTraversal(matrix, matrixElements);
+        assertArrayEquals(elements, matrixElements);
     }
 }
