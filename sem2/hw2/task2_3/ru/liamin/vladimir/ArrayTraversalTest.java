54320b60e8f1fe38475d2d53425cf55772749a7f
VLiamin <LyaminVA@yandex.ru>
2019-04-10 23:56:34 +0300

    spaces before brackets in ArrayTraversalTest.java

--- sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversalTest.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/ArrayTraversalTest.java
@@ -12,8 +12,8 @@ class ArrayTraversalTest {
         int[] elements = {2, 1, 2, 3, 4, 3, 2, 1, 0};
         int[] matrixElements = new int[9];
         int[][] matrix = new int[3][3];
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 matrix[i][j] = i + j;
             }
         }
