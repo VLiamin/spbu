9d08b0d295bcb9d40c2627e0a9b353f0f3060b57
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-02 00:51:00 +0300

    new documentation in HashQuickly.java

--- sem2/hw3/task3_1/ru/liamin/vladimir/HashQuickly.java
+++ sem2/hw3/task3_1/ru/liamin/vladimir/HashQuickly.java
@@ -1,14 +1,14 @@
 package ru.liamin.vladimir;
 
-/** Hash implementation class */
-public class HashQuickly implements Hash{
+/** Hash implementation class with function f(x) = x mod m */
+public class HashQuickly implements HashFunction {
 
     /**
      * Hash implementation method
      * @param number value of an element
      * @return hash of the element
      */
-    public int countHash(int number){
+    public int countHash(int number) {
         return number % 100;
     }
 }
