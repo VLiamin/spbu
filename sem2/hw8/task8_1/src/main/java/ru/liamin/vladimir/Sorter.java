718888442d9c741372a823d5b935ec8962cb7da4
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-11 18:33:15 +0300

    added documentation in Sorter.java

--- sem2/hw8/task8_1/src/main/java/ru/liamin/vladimir/Sorter.java
+++ sem2/hw8/task8_1/src/main/java/ru/liamin/vladimir/Sorter.java
@@ -3,5 +3,9 @@ package ru.liamin.vladimir;
 /** Interface for sorting */
 public interface Sorter {
 
+    /**
+     * Method which sort array
+     * @param numbers array which will be sorted
+     */
     void sort(int[] numbers);
 }
