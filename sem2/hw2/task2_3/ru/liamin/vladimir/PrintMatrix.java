6e0cf869d4a0cc79b7380bb595c96af2a7ac6fa4
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-18 15:12:36 +0300

    changed the name and parameters of methods

--- sem2/hw2/task2_3/ru/liamin/vladimir/PrintMatrix.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/PrintMatrix.java
@@ -3,11 +3,11 @@ package ru.liamin.vladimir;
 import java.io.IOException;
 
 /** Matrix output interface */
-public interface ArrayPrinting {
+public interface PrintMatrix {
     /**
      * Matrix derivation method
-     * @param elements array of matrix elements
+     * @param matrix matrix of elements that need to be rewritten
      */
-    void print(int[] elements) throws IOException;
+    void print(int[][] matrix) throws IOException;
 
 }
