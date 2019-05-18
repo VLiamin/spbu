687ff11785e5be6b3dd2a71e0339d36ed2d47455
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-18 15:04:17 +0300

    removed unnecessary global variables

--- sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiral.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiral.java
@@ -1,51 +1,51 @@
 package ru.liamin.vladimir;
 
+import java.io.PrintStream;
+
 /** Loop traversal class */
-public class ArrayTraversal {
-    private int column;
-    private int lines;
-    private int j;
-    private int count;
+public class BuildSpiral {
 
     /**
      * Method that writes elements from the matrix to the array
      * @param matrix source matrix of elements
      * @return array in which matrix elements are stored
      */
-    public void arrayTraversal(int[][] matrix, int[] elements) {
-        column = Math.round(matrix.length / 2);
-        lines = column;
-        j = 0;
-        elements[j] = matrix[lines][column];
+    public static void walkAroundTheMatrix(int[][] matrix, PrintStream printStream) {
+        int column = Math.round(matrix.length / 2);
+        int lines = column;
+        int j = 0;
+        int count = 0;
+
+        printStream.print(matrix[lines][column] + " ");
         j++;
         while ((column != 0) && (lines != 0))
         {
 
             column--;
-            elements[j] = matrix[lines][column];
+            printStream.print(matrix[lines][column] + " ");
             j++;
             for (int i = 0; i <= 0 + count * 2; i++)
             {
                 lines++;
-                elements[j] = matrix[lines][column];
+                printStream.print(matrix[lines][column] + " ");
                 j++;
             }
             for (int i = 0; i <= 1 + count * 2; i++)
             {
                 column++;
-                elements[j] = matrix[lines][column];
+                printStream.print(matrix[lines][column] + " ");
                 j++;
             }
             for (int i = 0; i <= 1 + count * 2; i++)
             {
                 lines--;
-                elements[j] = matrix[lines][column];
+                printStream.print(matrix[lines][column] + " ");
                 j++;
             }
             for (int i = 0; i <= 1 + count * 2; i++)
             {
                 column--;
-                elements[j] = matrix[lines][column];
+                printStream.print(matrix[lines][column] + " ");
                 j++;
             }
             count++;
