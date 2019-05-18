581c43463e35d9c1fd3a55faf94afa2a62b361b8
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-18 15:48:41 +0300

    used stream for console output

--- sem2/hw2/task2_3/ru/liamin/vladimir/PrintToConsole.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/PrintToConsole.java
@@ -1,15 +1,17 @@
 package ru.liamin.vladimir;
 
+import java.io.PrintStream;
+
 /** Class designed to display items on the console */
-public class PrintOnConsole implements Array {
+public class PrintToConsole implements PrintMatrix {
 
     /**
      * Displays the elements of the matrix to the console
-     * @param elements elements in the matrix when traversing it through the cycle
+     * @param matrix of elements that need to be rewritten
      */
-    public void print(int[] elements) {
-        for (int i = 0; i < elements.length; i++) {
-            System.out.print(elements[i] + " ");
-        }
+    public void print(int[][] matrix) {
+
+        PrintStream printStream = new PrintStream(System.out);
+        BuildSpiral.walkAroundTheMatrix(matrix, printStream);
     }
 }
