668a4da02f1611b462309500d627a533ca3979e6
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-18 15:55:09 +0300

    changed the test according to the change

--- sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiralTest.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiralTest.java
@@ -2,22 +2,26 @@ package ru.liamin.vladimir;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class ArrayTraversalTest {
+class BuildSpiralTest {
 
     @Test
     void arrayTraversal() {
-        ArrayTraversal arrayTraversal = new ArrayTraversal();
-        int[] elements = {2, 1, 2, 3, 4, 3, 2, 1, 0};
-        int[] matrixElements = new int[9];
+
+        ByteArrayOutputStream streamToCheck = new ByteArrayOutputStream();
+        String elements = "2 1 2 3 4 3 2 1 0 ";
+        System.setOut(new PrintStream(streamToCheck));
         int[][] matrix = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                matrix[i][j] = i + j;
+                matrix[i][j] = j + i;
             }
         }
-        arrayTraversal.arrayTraversal(matrix, matrixElements);
-        assertArrayEquals(elements, matrixElements);
+        BuildSpiral.walkAroundTheMatrix(matrix, System.out);
+        assertEquals(elements, streamToCheck.toString());
     }
 }
