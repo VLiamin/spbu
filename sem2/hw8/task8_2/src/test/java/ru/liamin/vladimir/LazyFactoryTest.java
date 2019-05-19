e64f56bb989a6d3232e0f88eaa607286b7ded276
Vladimir Liamin <LyaminVA@yandex.ru>
2019-05-19 18:47:46 +0300

    added race test

--- sem2/hw8/task8_2/src/test/java/ru/liamin/vladimir/LazyFactoryTest.java
+++ sem2/hw8/task8_2/src/test/java/ru/liamin/vladimir/LazyFactoryTest.java
@@ -2,7 +2,10 @@ package ru.liamin.vladimir;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class LazyFactoryTest {
 
@@ -31,9 +34,37 @@ class LazyFactoryTest {
     @Test
     void createMultithreadedLazy() {
 
-        Lazy<Integer> lazy = new LazyFactory().createSimpleLazy(() -> 341);
+        Lazy<Integer> lazy = new LazyFactory().createMultithreadedLazy(() -> 341);
         assertEquals(341, lazy.get());
         assertEquals(341, lazy.get());
     }
 
+    @Test
+    public void presenceOfRacesTest() throws InterruptedException {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        Lazy<Boolean> lazy = LazyFactory.createMultithreadedLazy(() -> {
+            numbers.add(87);
+            return true;
+        });
+
+        Runnable check = () -> {
+            for (int i = 0; i < 10; i++) {
+                lazy.get();
+            }
+        };
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            threads.add(new Thread(check));
+            threads.get(i).start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads.get(i).join();
+        }
+
+        assertEquals(1, numbers.size());
+        assertEquals(87, (int) numbers.get(0));
+    }
 }
