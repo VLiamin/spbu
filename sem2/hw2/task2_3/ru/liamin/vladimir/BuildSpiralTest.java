79ca0ebf5fe96880e760f2eaf1161935e8e99326
Vladimir Liamin <LyaminVA@yandex.ru>
2019-06-08 22:11:28 +0300

    changed test according to program changes

--- sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiralTest.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiralTest.java
@@ -2,26 +2,31 @@ package ru.liamin.vladimir;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BuildSpiralTest {
 
     @Test
-    void arrayTraversal() {
+    void print() throws IOException {
 
-        ByteArrayOutputStream streamToCheck = new ByteArrayOutputStream();
         String elements = "2 1 2 3 4 3 2 1 0 ";
-        System.setOut(new PrintStream(streamToCheck));
         int[][] matrix = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 matrix[i][j] = j + i;
             }
         }
-        BuildSpiral.walkAroundTheMatrix(matrix, System.out);
-        assertEquals(elements, streamToCheck.toString());
+        String file = "text.txt";
+        PrintStream printStream = null;
+        PrintMatrix printMatrix = new PrintToFile();
+        printMatrix.print(matrix, printStream);
+
+        FileReader in = new FileReader(file);
+        Scanner scanner = new Scanner(in);
+
+        assertEquals(elements, scanner.nextLine());
     }
 }
