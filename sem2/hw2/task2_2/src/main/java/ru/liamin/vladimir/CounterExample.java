5870281e964aea0c7732ac9073345da9d2646b55
Vladimir Liamin <LyaminVA@yandex.ru>
2019-06-08 17:44:50 +0300

    Fixed bug with overflow in CounterExample.java

--- sem2/hw2/task2_2/src/main/java/ru/liamin/vladimir/CounterExample.java
+++ sem2/hw2/task2_2/src/main/java/ru/liamin/vladimir/CounterExample.java
@@ -12,21 +12,21 @@ public class CounterExample {
         System.out.println("Your expression: ");
         String expression = ch.nextLine();
         Stack stack;
-        stack = new StackArray(5);
+        stack = new StackArray(expression.length());
         System.out.println("Stack by array: ");
         Calculate calculate = new Calculate(stack);
         char[] copyExpression = new char[expression.length()];
         for (int i = 0; i < expression.length(); i++) {
             copyExpression[i] = expression.charAt(i);
         }
-        System.out.println(calculate.countExpression(copyExpression));
+        System.out.println(calculate.calculate(copyExpression));
         stack = new StackPointer();
         System.out.println("Stack by pointer: ");
         calculate = new Calculate(stack);
         for (int i = 0; i < expression.length(); i++) {
             copyExpression[i] = expression.charAt(i);
         }
-        System.out.println(calculate.countExpression(copyExpression));
+        System.out.println(calculate.calculate(copyExpression));
 
     }
 }
