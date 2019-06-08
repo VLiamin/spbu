108190adca9150708925dab590002d9e63f85e8c
Vladimir Liamin <LyaminVA@yandex.ru>
2019-06-08 21:22:11 +0300

    redefined method print in PrintToConsole.java

--- sem2/hw2/task2_3/ru/liamin/vladimir/PrintToConsole.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/PrintToConsole.java
@@ -1,17 +1,17 @@
 package ru.liamin.vladimir;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 /** Class designed to display items on the console */
-public class PrintToConsole implements PrintMatrix {
+public class PrintToConsole extends BuildSpiral {
 
     /**
      * Displays the elements of the matrix to the console
      * @param matrix of elements that need to be rewritten
      */
-    public void print(int[][] matrix) {
-
-        PrintStream printStream = new PrintStream(System.out);
-        BuildSpiral.walkAroundTheMatrix(matrix, printStream);
+    public void print(int[][] matrix, PrintStream printStream) throws IOException {
+        printStream = new PrintStream(System.out);
+        super.print(matrix, printStream);
     }
 }
