64be5533a4bdb700d2ba04f5c707e65a443d1d8a
Vladimir Liamin <LyaminVA@yandex.ru>
2019-06-08 21:15:46 +0300

    suffered BuildSpiral.java to PrintMatrix.java

--- sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiral.java
+++ sem2/hw2/task2_3/ru/liamin/vladimir/BuildSpiral.java
@@ -1,16 +1,20 @@
 package ru.liamin.vladimir;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
-/** Loop traversal class */
-public class BuildSpiral {
+/**
+ * Loop traversal class
+ */
+public abstract class BuildSpiral implements PrintMatrix {
 
     /**
      * Method that writes elements from the matrix to the array
+     *
      * @param matrix source matrix of elements
      * @return array in which matrix elements are stored
      */
-    public static void walkAroundTheMatrix(int[][] matrix, PrintStream printStream) {
+    public void print(int[][] matrix, PrintStream printStream) throws IOException {
         int column = Math.round(matrix.length / 2);
         int lines = column;
         int j = 0;
@@ -18,32 +22,27 @@ public class BuildSpiral {
 
         printStream.print(matrix[lines][column] + " ");
         j++;
-        while ((column != 0) && (lines != 0))
-        {
+        while ((column != 0) && (lines != 0)) {
 
             column--;
             printStream.print(matrix[lines][column] + " ");
             j++;
-            for (int i = 0; i <= 0 + count * 2; i++)
-            {
+            for (int i = 0; i <= 0 + count * 2; i++) {
                 lines++;
                 printStream.print(matrix[lines][column] + " ");
                 j++;
             }
-            for (int i = 0; i <= 1 + count * 2; i++)
-            {
+            for (int i = 0; i <= 1 + count * 2; i++) {
                 column++;
                 printStream.print(matrix[lines][column] + " ");
                 j++;
             }
-            for (int i = 0; i <= 1 + count * 2; i++)
-            {
+            for (int i = 0; i <= 1 + count * 2; i++) {
                 lines--;
                 printStream.print(matrix[lines][column] + " ");
                 j++;
             }
-            for (int i = 0; i <= 1 + count * 2; i++)
-            {
+            for (int i = 0; i <= 1 + count * 2; i++) {
                 column--;
                 printStream.print(matrix[lines][column] + " ");
                 j++;
