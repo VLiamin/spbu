aa4509ed4b799a9e9b61527aefd4f367b4531166
VLiamin <LyaminVA@yandex.ru>
2019-06-09 01:03:24 +0300

    fixed MainApp for use of layouts

--- sem2/hw5/task5_1/src/main/java/ru/liamin/vladimir/MainApp.java
+++ sem2/hw5/task5_1/src/main/java/ru/liamin/vladimir/MainApp.java
@@ -1,15 +1,11 @@
 package ru.liamin.vladimir;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 /** Graphic application class */
 public class MainApp extends Application {
@@ -20,48 +16,17 @@ public class MainApp extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
-        Label label = new Label("Select Brightness:");
 
-        Label infoLabel = new Label("Value: 0.0");
 
-        Slider slider = new Slider();
-
-        ProgressBar progressBar = new ProgressBar(0);
-
-        slider.setMin(0);
-        slider.setMax(100);
-        slider.setValue(0);
-
-        slider.setShowTickLabels(true);
-        slider.setShowTickMarks(true);
-        slider.setBlockIncrement(0.1);
-
-        // Adding Listener to value property.
-        slider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            /**
-             * Class changing progress bar
-             * @param observable the ObservableValue which value changed
-             * @param oldValue the old value of progress bar
-             * @param newValue the new value of progress bar
-             */
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-
-                infoLabel.setText("Value: " + newValue);
-                progressBar.setProgress((Double) newValue / 100);
-            }
-        });
-
-        VBox root = new VBox();
-        root.setPadding(new Insets(20));
-        root.setSpacing(10);
-        root.getChildren().addAll(label, slider, infoLabel, progressBar);//, progressBar);
-
+        Parent root = FXMLLoader.load(getClass().getResource("/ru.liamin.vladimir/scene.fxml"));
         stage.setTitle("JavaFX Slider (ru.liamin.vladimir)");
         Scene scene = new Scene(root, 350, 200);
+
+        stage.setMinWidth(100);
+        stage.setMinHeight(100);
         stage.setScene(scene);
         stage.show();
+
     }
 
     /**
@@ -69,7 +34,7 @@ public class MainApp extends Application {
      * @param args args array of arguments
      */
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 
 }
