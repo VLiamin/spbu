38f7904608cea3d142485be20c6dd9b496efad8c
VLiamin <LyaminVA@yandex.ru>
2019-06-09 17:44:27 +0300

    fixed documentation and redone for layouts in MainApp.java

--- sem2/hw5/task5_2/src/main/java/ru/liamin/vladimir/MainApp.java
+++ sem2/hw5/task5_2/src/main/java/ru/liamin/vladimir/MainApp.java
@@ -1,121 +1,34 @@
 package ru.liamin.vladimir;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.control.*;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 /** Graphic application class */
 public class MainApp extends Application {
 
     /**
      * Graphical application method
-     * @param primaryStage variable for the GUI window
-     * @throws Exception exception of division by zero
+     * @param stage variable for the GUI window
+     * @throws Exception input / output exception
      */
     @Override
-    public void start(Stage primaryStage) throws ArithmeticException {
+    public void start(Stage stage) throws IOException {
 
-        ArithmeticSign subtraction = new ArithmeticSign('-', "-");
-        ArithmeticSign addition = new ArithmeticSign('+', "+");
-        ArithmeticSign multiplication = new ArithmeticSign('*', "*");
-        ArithmeticSign division = new ArithmeticSign('/', "/");
+        Parent root = FXMLLoader.load(getClass().getResource("/ru.liamin.vladimir/scene.fxml"));
+        stage.setTitle("JavaFX Slider (ru.liamin.vladimir)");
+        Scene scene = new Scene(root, 550, 200);
 
-        FlowPane root = new FlowPane();
+        stage.setMinWidth(100);
+        stage.setMinHeight(100);
+        stage.setScene(scene);
+        stage.show();
 
-        ObservableList<ArithmeticSign> sign
-                = FXCollections.observableArrayList(subtraction, addition , multiplication, division);
-
-        ChoiceBox<ArithmeticSign> choiceBox = new ChoiceBox<>(sign);
-        choiceBox.setValue(subtraction);
-        final Spinner<Integer> spinner1 = new Spinner<>();
-
-        final int initialValue = 3;
-
-        SpinnerValueFactory<Integer> valueFactory = //
-                new SpinnerValueFactory.IntegerSpinnerValueFactory(-100, 100, initialValue);
-
-        spinner1.setValueFactory(valueFactory);
-
-        final Spinner<Integer> spinner2 = new Spinner<>();
-
-        valueFactory =
-                new SpinnerValueFactory.IntegerSpinnerValueFactory(-100, 100, initialValue);
-
-        spinner2.setValueFactory(valueFactory);
-
-        Label label = new Label("=");
-        TextField textField = new TextField("");
-        textField.setMinWidth(170);
-
-        Calculate calculate = new Calculate();
-        textField.setText("0");
-        /** Handles the change in the first spinner */
-        spinner1.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-
-                try {
-                    textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), String.valueOf(choiceBox.getValue())));
-                } catch (ArithmeticException e) {
-                    textField.setText("Division by zero.");
-                }
-            }
-        });
-
-        /** Handles the change in the second spinner */
-        spinner2.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-
-                try {
-                    textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), String.valueOf(choiceBox.getValue())));
-                } catch (ArithmeticException e) {
-                    textField.setText("Division by zero.");
-                }
-            }
-        });
-
-        /** Handles the change in the choiceBox*/
-        choiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue observable, Number oldValue, Number newValue) {
-                if (newValue.equals(0))
-                    textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), "-"));
-                else if (newValue.equals(1))
-                    textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), "+"));
-                else if (newValue.equals(2))
-                    textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), "*"));
-                else
-                    try {
-                        textField.setText(calculate.count(spinner1.getValue(), spinner2.getValue(), "/"));
-                    } catch (ArithmeticException e) {
-                        textField.setText("Division by zero.");
-                    }
-
-            }
-        });
-
-        root.setPadding(new Insets(100));
-        root.setHgap(10);
-        root.setVgap(10);
-        root.setPadding(new Insets(10));
-        root.getChildren().addAll(spinner1, choiceBox, spinner2, label, textField);
-        root.setPadding(new Insets(20));
-        root.setHgap(10);
-
-        primaryStage.setTitle("ChoiceBox (ru.liamin.vladimir)");
-        Scene scene = new Scene(root, 700, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
@@ -123,7 +36,7 @@ public class MainApp extends Application {
      * @param args args array of arguments
      */
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 
 }
