c561cd69d51d5a9f3e53ec6c2cc6077be03b3e26
VLiamin <LyaminVA@yandex.ru>
2019-06-10 21:49:37 +0300

    check done in a generalized way in Victory.java

--- sem2/hw6/task6_2/src/main/java/ru/liamin/vladimir/Victory.java
+++ sem2/hw6/task6_2/src/main/java/ru/liamin/vladimir/Victory.java
@@ -1,35 +1,33 @@
 package ru.liamin.vladimir;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.util.ArrayList;
+
 import static java.lang.Math.abs;
+import static java.lang.StrictMath.sqrt;
 
+/** Class that checks the game for the presence of a winner */
 public class Victory {
-    private int total;
 
-    public boolean win(int[] numbers, TextArea textArea, Button buttonOne, Button buttonTwo, Button buttonThree, Button buttonFour,
-                       Button buttonFive, Button buttonSix, Button buttonSeven, Button buttonEight, Button buttonNine, int count) {
-        if ((abs(numbers[0] + numbers[1] + (numbers[2])) == 3) ||
-                (abs(numbers[3] + numbers[4] + numbers[5]) == 3) ||
-                (abs(numbers[6] + numbers[7] + numbers[8]) == 3) ||
-                (abs(numbers[2] + numbers[4] + numbers[6]) == 3) ||
-                (abs(numbers[0] + numbers[4] + numbers[8]) == 3) ||
-                (abs(numbers[0] + numbers[3] + numbers[6]) == 3) ||
-                (abs(numbers[1] + numbers[4] + numbers[7]) == 3) ||
-                (abs(numbers[3] + numbers[5] + numbers[8]) == 3)) {
-            for (int i = 0; i < numbers.length; i++) {
-                numbers[i] = 0;
-            }
-            buttonOne.setText("");
-            buttonTwo.setText("");
-            buttonThree.setText("");
-            buttonFour.setText("");
-            buttonFive.setText("");
-            buttonSix.setText("");
-            buttonSeven.setText("");
-            buttonEight.setText("");
-            buttonNine.setText("");
+    /**
+     * Method that checks the game is finished or not
+     * @param numbers array of player turns
+     * @param textArea textArea where results will be written
+     * @param buttons buttons of the game
+     * @param count whose turn was the last
+     * @return is the game finished or not
+     */
+    public boolean win(int[] numbers, TextArea textArea, ArrayList<Button> buttons, int count) {
+        int total = 0;
+        if ((checkForVictory(numbers, 1, (int) sqrt(numbers.length))) ||
+                (checkForVictory(numbers, (int) sqrt(numbers.length) - 1, 0)) ||
+                (checkForVictory(numbers, (int) sqrt(numbers.length) + 1, 0)) ||
+                (checkForVictory(numbers, (int) sqrt(numbers.length), 1))) {
+
+            clearField(numbers, buttons);
             if (count % 2 == 1)
                 textArea.setText("X win!");
             else
@@ -37,26 +35,55 @@ public class Victory {
             return true;
 
         }
-        total = 0;
+
         for (int i = 0; i < numbers.length; i++) {
             total = total + abs(numbers[i]);
         }
 
         if (total == 9) {
-            for (int i = 0; i < numbers.length; i++) {
-                numbers[i] = 0;
-            }
-            buttonOne.setText("");
-            buttonTwo.setText("");
-            buttonThree.setText("");
-            buttonFour.setText("");
-            buttonFive.setText("");
-            buttonSix.setText("");
-            buttonSeven.setText("");
-            buttonEight.setText("");
-            buttonNine.setText("");
-            textArea.setText("Draw");
+            textArea.setText("Draw!");
+            clearField(numbers, buttons);
             return true;
+        }
+        return false;
+    }
+
+    private void clearField(int[] numbers, ArrayList<Button> buttons) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = 0;
+        }
+
+        for (Button button : buttons) {
+
+            button.setText("");
+        }
+    }
+
+    /**
+     * Method that checks the game for the presence of a winner
+     * @param numbers array of player turns
+     * @param numberOfCellsToSkip how many cells should be skipped at each iteration of the cycle
+     * @param nextCell the cell from where the next check will be started
+     * @return is there a winner or not
+     */
+    public boolean checkForVictory(int[] numbers, int numberOfCellsToSkip, int nextCell) {
+
+        int trickCycles = (int) sqrt(numbers.length);
+        if (nextCell == 0)
+            trickCycles = 1;
+        int start = 0;
+        if (numberOfCellsToSkip == (int) sqrt(numbers.length) - 1)
+            start = (int) sqrt(numbers.length) - 1;
+        for (int i = 0; i < trickCycles; i++) {
+            int count = 0;
+            for (int j = nextCell * i + start; j < nextCell * i + numberOfCellsToSkip * (int) sqrt(numbers.length) + start; j = j + numberOfCellsToSkip) {
+
+                count = count + numbers[j];
+            }
+
+            if (abs(count) == 3)
+                return true;
         }
         return false;
     }
