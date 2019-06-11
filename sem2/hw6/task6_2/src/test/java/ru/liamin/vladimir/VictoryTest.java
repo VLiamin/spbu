ca88463b7fedc5f0e8e1b807835301cc0761e4bb
VLiamin <LyaminVA@yandex.ru>
2019-06-11 15:03:06 +0300

    added new tests

--- sem2/hw6/task6_2/src/test/java/ru/liamin/vladimir/VictoryTest.java
+++ sem2/hw6/task6_2/src/test/java/ru/liamin/vladimir/VictoryTest.java
@@ -7,11 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 class VictoryTest {
 
     @Test
-    void checkForVictory() {
+    void checkForVictoryInTheMiddleOfGame() {
 
         Victory victory = new Victory();
         int[] numbers = {1, -1, 1, 1, -1, -1, 1, 0 ,0};
         assertEquals(true, victory.checkForVictory(numbers, 3, 1));
+        assertEquals(false, victory.checkForVictory(numbers, 1, 3));
+        assertEquals(false, victory.checkForVictory(numbers, 4, 0));
+        assertEquals(false, victory.checkForVictory(numbers, 2, 0));
+    }
+
+    @Test
+    void checkForVictoryInTheEndOfGame() {
+
+        Victory victory = new Victory();
+        int[] numbers = {1, -1, -1, -1, 1, -1, 1, 1, 1};
+        assertEquals(false, victory.checkForVictory(numbers, 3, 1));
+        assertEquals(true, victory.checkForVictory(numbers, 1, 3));
+        assertEquals(true, victory.checkForVictory(numbers, 4, 0));
+        assertEquals(false, victory.checkForVictory(numbers, 2, 0));
+    }
+
+    @Test
+    void checkForVictoryInTheBeginOfGame() {
+
+        Victory victory = new Victory();
+        int[] numbers = {1, -1, -1, 0, 0, 0, 0, 0, 0};
+        assertEquals(false, victory.checkForVictory(numbers, 3, 1));
         assertEquals(false, victory.checkForVictory(numbers, 1, 3));
         assertEquals(false, victory.checkForVictory(numbers, 4, 0));
         assertEquals(false, victory.checkForVictory(numbers, 2, 0));
