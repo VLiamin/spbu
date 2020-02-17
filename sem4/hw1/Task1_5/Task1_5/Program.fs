ead73f0034d63476d26c60562a845a1e7cf4fccc
VLiamin <LyaminVA@yandex.ru>
2020-02-18 01:58:03 +0300

    added Task1_5

---
+++ sem4/hw1/Task1_5/Task1_5/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec getFirstPosition acc number list = 
+ if List.head list = number then
+  acc
+ else
+  getFirstPosition
+   (acc + 1)
+   (number)
+   (List.tail list)
+printfn "The number of first position: %d" (getFirstPosition 0 3 [1; 2; 3]) 
