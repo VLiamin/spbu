9516e06f1e36d640fc8d3f610056e4c83c1c7cc7
VLiamin <LyaminVA@yandex.ru>
2020-02-18 01:50:15 +0300

    added Task1_4

---
+++ sem4/hw1/Task1_4/Task1_4/Program.fs
@@ -0,0 +1,20 @@
+﻿let rec countNumber acc digit = 
+ if digit = 0 
+ then 
+  acc
+ else 
+  countNumber 
+   (acc * 2)
+   (digit - 1)
+
+let rec makeList acc n m = 
+ if m = -1 then
+  acc
+ else
+  makeList
+   (countNumber (1) (n + m) :: acc)
+   (n)
+   (m - 1)
+
+let list = makeList ([]) (1) (6)
+printfn "Our list is: %A" list
