35f5a19db189bd77512d1527b1b582a133370210
VLiamin <LyaminVA@yandex.ru>
2020-02-18 01:42:56 +0300

    added Task1_2

---
+++ sem4/hw1/Task1_2/Task1_2/Program.fs
@@ -0,0 +1,14 @@
+﻿let list = [1; 0]
+
+let rec countFibonacci list n i = 
+ if (n = 1) || (n = 0) then
+  1
+ else
+  if n = i then
+   List.head list
+  else
+   countFibonacci
+    (List.head list + List.nth list 1 :: list) 
+    (n)
+    (i + 1)
+printfn "Value: %d" (countFibonacci list 5 1)
