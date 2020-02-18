fc8ef22868fdeed0680a3db5316837c6362268b4
VLiamin <LyaminVA@yandex.ru>
2020-02-19 00:30:57 +0300

    added Task1_3

---
+++ sem4/hw1/Task1_3/Task1_3/Program.fs
@@ -0,0 +1,13 @@
+﻿let makeReverse list = 
+    if List.length list = 0 then
+        list
+    else
+        let rec doRecursion list newList = 
+            if List.length list = 0 then
+                newList
+            else 
+                doRecursion (List.tail list) (List.head list :: newList)
+        
+        doRecursion list []
+        
+printfn "New list: %A" (makeReverse [1; 2; 3; 7]) 
