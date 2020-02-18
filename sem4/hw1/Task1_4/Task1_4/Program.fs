c2701a4eefcfc1cc993e66ed22d443231dd4eb24
VLiamin <LyaminVA@yandex.ru>
2020-02-18 21:47:02 +0300

    fixed array filling and variable names

--- sem4/hw1/Task1_4/Task1_4/Program.fs
+++ sem4/hw1/Task1_4/Task1_4/Program.fs
@@ -1,20 +1,26 @@
-﻿let rec countNumber acc digit = 
- if digit = 0 
- then 
-  acc
+﻿let rec countPowerOfTwo listOfPowers power = 
+ if power = 0 then 
+  listOfPowers
  else 
-  countNumber 
-   (acc * 2)
-   (digit - 1)
+  countPowerOfTwo 
+   (List.head listOfPowers * 2 :: listOfPowers)
+   (power - 1)
 
-let rec makeList acc n m = 
- if m = -1 then
-  acc
+let makeList listOfOurElements n m = 
+ if m <= -1 || n < 0 then
+  printfn "Incorrect data given"
+  listOfOurElements
  else
-  makeList
-   (countNumber (1) (n + m) :: acc)
-   (n)
-   (m - 1)
-
-let list = makeList ([]) (1) (6)
+  let rec doRecursion numberOfElemets list acc = 
+   if numberOfElemets <= 0 then
+    acc
+   else
+    doRecursion
+     (numberOfElemets - 1)
+     (List.tail list)
+     (List.head list :: acc)
+  let listOfPowersOfTwo = countPowerOfTwo [1] (n + m)
+  doRecursion (m + 1) listOfPowersOfTwo listOfOurElements
+ 
+let list = makeList [] 1 7
 printfn "Our list is: %A" list
