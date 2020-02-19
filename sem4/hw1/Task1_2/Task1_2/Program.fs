b4099cae3294f511c996a35ecea89b96b1464700
VLiamin <LyaminVA@yandex.ru>
2020-02-19 09:03:33 +0300

    registration is corrected and processing of incorrect data is added

--- sem4/hw1/Task1_2/Task1_2/Program.fs
+++ sem4/hw1/Task1_2/Task1_2/Program.fs
@@ -1,14 +1,17 @@
 ﻿let countFibonacci number = 
- if (number = 1) || (number = 0) then
-   number
- else
-  let rec doRecursion counter n1 n2 = 
-   if counter >= number then
-    n1
-   else
-    doRecursion
-     (counter + 1)
-     (n1 + n2) 
-     n1
-  doRecursion 2 1 1
-printfn "Value: %d" (countFibonacci 7)
+    if number < 0 then
+        printfn "Incorrect data"
+        None
+    else
+        if (number = 1) || (number = 0) then
+            Some (number)
+        else
+            let rec doRecursion counter n1 n2 = 
+                if counter >= number then
+                    n1
+                else
+                    doRecursion (counter + 1) (n1 + n2) n1
+
+            Some (doRecursion 2 1 1)
+
+printfn "Value : %A" (countFibonacci 5)
