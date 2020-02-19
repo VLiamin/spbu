c429609b9dba40f3c1bac1996748f09d1890e4aa
VLiamin <LyaminVA@yandex.ru>
2020-02-19 13:53:54 +0300

    fixed design

--- sem4/hw1/Task1_4/Task1_4/Program.fs
+++ sem4/hw1/Task1_4/Task1_4/Program.fs
@@ -1,26 +1,22 @@
 ﻿let rec countPowerOfTwo listOfPowers power = 
- if power = 0 then 
-  listOfPowers
- else 
-  countPowerOfTwo 
-   (List.head listOfPowers * 2 :: listOfPowers)
-   (power - 1)
+    if power = 0 then 
+        listOfPowers
+    else 
+        countPowerOfTwo (List.head listOfPowers * 2 :: listOfPowers) (power - 1)
 
 let makeList listOfOurElements n m = 
- if m <= -1 || n < 0 then
-  printfn "Incorrect data given"
-  listOfOurElements
- else
-  let rec doRecursion numberOfElemets list acc = 
-   if numberOfElemets <= 0 then
-    acc
-   else
-    doRecursion
-     (numberOfElemets - 1)
-     (List.tail list)
-     (List.head list :: acc)
-  let listOfPowersOfTwo = countPowerOfTwo [1] (n + m)
-  doRecursion (m + 1) listOfPowersOfTwo listOfOurElements
+    if m <= -1 || n < 0 then
+        printfn "Incorrect data given"
+        listOfOurElements
+    else
+        let rec doRecursion numberOfElemets list acc = 
+            if numberOfElemets <= 0 then
+                acc
+            else
+                doRecursion (numberOfElemets - 1) (List.tail list) (List.head list :: acc)
+
+        let listOfPowersOfTwo = countPowerOfTwo [1] (n + m)
+        doRecursion (m + 1) listOfPowersOfTwo listOfOurElements
  
-let list = makeList [] 1 7
+let list = makeList [] 1 8
 printfn "Our list is: %A" list
