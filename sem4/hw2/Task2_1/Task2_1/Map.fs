bf0cc45888f25628f7ffaa2ba25cbf0a9e602d42
VLiamin <LyaminVA@yandex.ru>
2020-02-26 20:40:13 +0300

    added Task2_1

---
+++ sem4/hw2/Task2_1/Task2_1/Map.fs
@@ -0,0 +1,9 @@
+﻿module CountEvenNumbersWithMap
+
+let countEvenNumbersWithMap list =
+    let newList = List.sort (List.map (fun x -> x % 2 * x) list)
+    let isNotZero x = x > 0
+
+    match List.tryFindIndex isNotZero newList with
+    | Some value -> value
+    | None -> List.length newList
