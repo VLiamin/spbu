cb47bc45b88ff9fc313dfa992aa3b20e86037a14
VLiamin <LyaminVA@yandex.ru>
2020-02-26 19:45:47 +0300

    added Task2_1

---
+++ sem4/hw2/Task2_1/Task2_1/Filter.fs
@@ -0,0 +1,7 @@
+﻿module CountEvenNumbersWithFilter
+
+let countEvenNumbersWithFilter list =
+    List.length (List.filter (fun x -> x % 2 = 0) list)
+  
+printfn "Value: %d" (countEvenNumbersWithFilter [1; 3; 4; 5; 6; 7; 8; 9])
+
---
+++ sem4/hw2/Task2_1/Task2_1/Folder.fs
@@ -0,0 +1,5 @@
+﻿module CountEvenNumbersWithFold
+
+let countEvenNumbersWithFold list =
+    List.fold (fun acc x -> acc + abs (x + 1) % 2) 0 list
+
