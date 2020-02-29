578a9157008ad5184f50e9d62eb2b7084f32fcf3
VLiamin <LyaminVA@yandex.ru>
2020-02-29 18:24:57 +0300

    added Task2_4

---
+++ sem4/hw2/Task2_4/Task2_4/CountPrime.fs
@@ -0,0 +1,12 @@
+﻿module CountSimpleNumbers
+
+let isPrime x = 
+    let rec check i =
+        x > 1 && i > x / 2 || (x % i <> 0 && check (i + 1))
+    match x with
+    | x when x <= 1 -> false
+    | _ -> check 2
+
+let returnAnInfiniteSequenceOfOrimes () = 
+    Seq.initInfinite (fun number -> number) 
+    |> Seq.filter (fun number -> isPrime number)
