54e6aa3b84c05f243971553a21388aa1f4820189
VLiamin <LyaminVA@yandex.ru>
2020-02-29 14:23:04 +0300

    added Task2_4

---
+++ sem4/hw2/Task2_4/Tests2_4/UnitTest1.fs
@@ -0,0 +1,29 @@
+module Tests2_4
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open CountSimpleNumbers
+
+
+let testCases = 
+    [
+        2, true
+        4, false
+        1, false
+        10, false
+        13, true
+        1001, true
+    ] |> List.map (fun (number, numberIsPrime) -> TestCaseData(number, numberIsPrime))
+
+[<Test>]
+[<TestCaseSource("testCases")>]
+let isPrimeTest number numberIsPrime =
+    Check.Quick (fun (int) -> (isPrime number) = numberIsPrime)
+
+
+
+[<Test>]
+let returnAnInfiniteSequenceOfOrimesTest1 () =
+    let seq = returnAnInfiniteSequenceOfOrimes ()
+    Seq.take 6 seq |> should equal [2; 3; 5; 7; 11; 13; 17]
