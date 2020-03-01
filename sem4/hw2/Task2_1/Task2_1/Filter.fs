a04f3bda47a840b810a34fd243e25dfcec2671bd
VLiamin <LyaminVA@yandex.ru>
2020-03-01 18:06:49 +0300

    added task2_1

---
+++ sem4/hw2/Task2_1/Task2_1/Filter.fs
@@ -0,0 +1,6 @@
+﻿module CountEvenNumbersWithFilter
+// Function that counts the number of even elements in a list using filter
+let countEvenNumbersWithFilter list =
+    List.length (List.filter (fun x -> x % 2 = 0) list)
+ 
+
---
+++ sem4/hw2/Task2_1/Task2_1/Map.fs
@@ -0,0 +1,9 @@
+﻿module CountEvenNumbersWithMap
+// Function that counts the number of even elements in a list using map
+let countEvenNumbersWithMap list =
+    let newList = List.sort (List.map (fun x -> x % 2 * x) list)
+    let isNotZero x = x > 0
+
+    match List.tryFindIndex isNotZero newList with
+    | Some value -> value
+    | None -> List.length newList
