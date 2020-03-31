355d3534bda6d389cc2924648d361fc7d3f285b2
VLiamin <LyaminVA@yandex.ru>
2020-04-01 02:28:44 +0300

    added task6_1

---
+++ sem4/hw6/Task6_1/Task6_1/Program.fs
@@ -0,0 +1,19 @@
+﻿module Task6_1
+open System
+// Builder which considers the expression
+type Rounding(round) =
+    member this.Bind(x, f) = 
+        f x
+    member this.Return(x) = Math.Round((x :float), (round : int))  
+let rounding round = new Rounding(round)
+
+// Function which considers the expression
+let count round firstValue secondValue thirdValue = 
+    rounding round {
+        let! a = firstValue / secondValue
+        let! b = thirdValue
+        return a / b
+    }
+
+let main = 
+    printfn "%f" (count 3 2.0 12.0 3.5)
---
+++ sem4/hw6/Task6_1/Test6_1/UnitTest1.fs
@@ -0,0 +1,30 @@
+module Test6_1
+
+open NUnit.Framework
+open Task6_1
+open FsUnit
+let testCases = 
+    [
+        1, 1.0, 10.0, 1.0, 0.1
+        2, 3.0, 9.0, 2.0, 0.17
+        3, 5.0, 3.0, 4.0, 0.417
+        2, 3.5, 2.1, 7.1, 0.23 
+    ] |> List.map (fun (round, firstValue, secondValue, thirdValue, result) -> 
+                                               TestCaseData(round, firstValue, secondValue, thirdValue, result))
+
+
+[<Test>]
+[<TestCaseSource("testCases")>]
+let checkCount round firstValue secondValue thirdValue result =
+    count round firstValue secondValue thirdValue |> should equal result
+
+let count2 () = 
+    rounding 4 {
+        let! a = 2.1 * 3.78
+        let! b = 5.0 / 8.9
+        let! c = 2.1
+        return a * b / c
+    }
+[<Test>]
+let checkCount2  =
+    count2 () |> should equal 2.1236
