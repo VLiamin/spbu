74cca760b3e1935cd7b7ddef985e8859050ab2f4
VLiamin <LyaminVA@yandex.ru>
2020-04-02 00:07:41 +0300

    task6_2 added

---
+++ sem4/hw6/Task_6_2/Task_6_2/Program.fs
@@ -0,0 +1,25 @@
+﻿module Task_6_2
+open System
+// Builder which considers the expression
+type Calculate() =
+    member this.Bind(x, f) =
+        try
+            f (int x)
+        with
+        | msg ->  "Your expression is not right"
+    member this.Return(x) = (string x)
+    
+let calculate () = new Calculate()
+
+// Function which shows the work of builder
+let countResult firstValue secondValue = 
+    calculate () {
+        let! x = firstValue
+        let! y = secondValue
+        let z = x + y
+        return z
+    }
+
+let main = 
+    printfn "%s" (countResult "1y" "2")
+
