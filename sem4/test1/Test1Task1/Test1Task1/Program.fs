4fd62861422b1ed09895ea661a9e6d7890adb522
VLiamin <LyaminVA@yandex.ru>
2020-04-10 17:37:47 +0300

    files added

---
+++ sem4/test1/Test1Task1/Test1Task1/Program.fs
@@ -0,0 +1,11 @@
+﻿module Task1
+
+let findMin list =
+    let newList = List.sort list
+    if (List.length newList > 0) then
+        Some (List.head newList)
+    else
+        None
+printfn "%A" (findMin [2])
+
+
