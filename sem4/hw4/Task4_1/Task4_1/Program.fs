081f32d385fea871ce59c5d56bfb15dd8a39d501
VLiamin <LyaminVA@yandex.ru>
2020-05-09 19:39:34 +0300

    removed copy-paste

--- sem4/hw4/Task4_1/Task4_1/Program.fs
+++ sem4/hw4/Task4_1/Task4_1/Program.fs
@@ -1,24 +1,26 @@
 ﻿module Task4_1
 
+/// Function that returns a closing bracket of the same type
+let getOpposite x = 
+   match x with
+   | '(' -> ')'
+   | '[' -> ']'
+   | '{' -> '}'
+   | x -> x
+
 /// Function that checks the string for correct brackets 
 let checkParentheses line =
     /// Function that goes along the line and counts the brackets
     let rec lineTraversal newLine list =
         if (List.length newLine = 0) then
             List.length list = 0
-        elif (List.length list > 0 && (List.head newLine).Equals(List.head list) = false || List.length list = 0) && 
-             (List.head newLine = ')' || List.head newLine = ']' || List.head newLine = '}') then
-            false
-        elif (List.head newLine = ')' || List.head newLine = ']' || List.head newLine = '}') && (List.length list = 0) then
+        elif (List.head newLine = ')' || List.head newLine = ']' || List.head newLine = '}') && 
+             (List.length list > 0 && (List.head newLine).Equals(List.head list) = false || List.length list = 0) then
             false
         else 
             match (List.head newLine) with
-            | '(' -> lineTraversal (List.tail newLine) (')' :: list)
-            | ')' -> lineTraversal (List.tail newLine) (List.tail list)
-            | '[' -> lineTraversal (List.tail newLine) (']' :: list)
-            | ']' -> lineTraversal (List.tail newLine) (List.tail list)
-            | '{' -> lineTraversal (List.tail newLine) ('}' :: list)
-            | '}' -> lineTraversal (List.tail newLine) (List.tail list)
+            | x when x = '(' || x = '[' || x = '{' -> lineTraversal (List.tail newLine) (getOpposite x :: list)
+            | x when x = ')' || x = '}' || x = ']' -> lineTraversal (List.tail newLine) (List.tail list)
             | _ -> lineTraversal (List.tail newLine) list
     lineTraversal (Seq.toList(line)) []
 
