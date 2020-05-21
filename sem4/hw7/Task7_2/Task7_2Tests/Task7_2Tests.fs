49480b2336e0b5881c88c22bb28c2606ade8e475
VLiamin <LyaminVA@yandex.ru>
2020-05-22 02:58:48 +0300

    test changed

--- sem4/hw7/Task7_2/Task7_2Tests/Task7_2Tests.fs
+++ sem4/hw7/Task7_2/Task7_2Tests/Task7_2Tests.fs
@@ -1,6 +1,5 @@
 ﻿module HtmlCount.Tests
 
-open HtmlCount
 open NUnit.Framework
 open FsUnit
 open Foq
@@ -25,30 +24,27 @@ let singleAddressTest () =
 
 [<Test>]
 let multipleAddressTest () =
-    let page1 = async { return "1" }
+
     let page2 = async { return "12" }
     let page3 = async { return "123" }
     let page4 = async { return "1234" }
 
-    let html = "<a href=\"http://first.ru/\" />
-        <a href=\"http://second.ru/\" />
+    let html = "<a href=\"http://second.ru/\" />
         <a href=\"http://third.ru/\" />
-        <a href=\"http://fourth.ru/\" />
-        <a href=\"http://fifth.ru/\" />"
-    let main = async { return html }
+        <a href=\"http://fourth.ru/\" />"
+
+    let mainPage = async { return html }
 
     let mock =
         Mock<IHtmlRequest>()
-            .Setup(fun x -> <@ x.AccessHtmlAsync("main") @>).Returns(main)
-            .Setup(fun x -> <@ x.AccessHtmlAsync("http://first.ru/") @>).Returns(page1)
+            .Setup(fun x -> <@ x.AccessHtmlAsync("main") @>).Returns(mainPage)
             .Setup(fun x -> <@ x.AccessHtmlAsync("http://second.ru/") @>).Returns(page2)
             .Setup(fun x -> <@ x.AccessHtmlAsync("http://third.ru/") @>).Returns(page3)
-            .Setup(fun x -> <@ x.AccessHtmlAsync("http://fourth.ru/") @>).Returns(page4).Create()
-
+            .Setup(fun x -> <@ x.AccessHtmlAsync("http://fourth.ru/") @>).Returns(page4)
+            .Create()
     let processor = HtmlProcessor(mock)
     let result = Async.RunSynchronously(processor.CountLinkedPagesSymbols "main")
-    result.Length |> should equal 5
-    result |> should contain ("http://first.ru/", 1)
+    result.Length |> should equal 3
     result |> should contain ("http://second.ru/", 2)
     result |> should contain ("http://third.ru/", 3)
     result |> should contain ("http://fourth.ru/", 4)
