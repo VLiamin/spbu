050c192561a87ba477f9e74c5973d027221930a6
VLiamin <LyaminVA@yandex.ru>
2020-06-05 21:57:19 +0300

    added Task7_1

---
+++ sem4/hw7/Task7_1/Task7_1/Lazy.fs
@@ -0,0 +1,13 @@
+﻿namespace Lazy
+
+type Lazy<'a> (supplier : unit -> 'a) =
+    
+    let mutable result = None
+
+    interface ILazy<'a> with
+
+        member this.Get () =
+            match result with
+            | Some (x) -> x
+            | None -> result <- Some (supplier())
+                      result.Value
